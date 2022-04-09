c7911281c3a9f8bfae3c9c82744ddf7390fdc568
evonneng <evonne_ng@berkeley.edu>
2022-04-09 05:26:22 +0000

    fixing documentation for unfinished module ordering function

--- mattport/nerf/graph.py
+++ mattport/nerf/graph.py
@@ -15,11 +15,7 @@ class GraphInputs:
 
 
 class Graph(nn.Module):
-    """_summary_
-
-    Args:
-        nn (_type_): _description_
-    """
+    """_summary_ """
 
     def __init__(self, modules_config: dict) -> None:
         super().__init__()
@@ -34,17 +30,16 @@ class Graph(nn.Module):
                 module_dict["meta_data"]["in_dim"] = in_dim
             self.modules[module_name] = module(**module_dict["meta_data"])
 
-    def get_module_order(self, modules_config: list):
-        """Generates a graph to determine the order in which the graph should execute
+    def get_module_order(self, modules_config: dict) -> list:
+        """Generates a graph to determine the order in which the modules should execute
 
         Args:
-            modules_config (list): _description_
+            modules_config (dict): module definitions that make up the network
 
         Returns:
-            _type_: _description_
+            list: ordering of the module names that should be executed
         """
-        # raise NotImplementedError
-        return modules_config
+        raise NotImplementedError
 
     def forward(self, x):
         """_summary_
@@ -52,6 +47,6 @@ class Graph(nn.Module):
         Args:
             x (_type_): _description_
         """
-        # raise NotImplementedError
-        for name in self.module_order:
-            x = self.modules[name](x)
+        # for name in self.module_order:
+        #     x = self.modules[name](x)
+        raise NotImplementedError
