1dc574281ca2c605c5e2ce0c270195ad53a6ea40
evonneng <evonneng@users.noreply.github.com>
2022-05-11 17:56:39 -0700

    Added eval script for benchmarking blender dataset (#71)
    * fixed train iteration to much longer
    
    * done adding test benchmark script

--- scripts/benchmarking/launch_train_blender.sh
+++ scripts/benchmarking/launch_train_blender.sh
@@ -7,6 +7,9 @@ for dataset in ${DATASETS[@]}; do
     python scripts/run_train.py \
            data.dataset.data_directory=data/blender/${dataset} \
            experiment_name=blender_${dataset} \
+           graph.model_dir=mattport_models/ \
+           graph.steps_per_save=25000 \
+           graph.max_num_iterations=2000000 \
            logging.enable_stats=False \
            logging.enable_profiler=False &
     echo "Launched ${dataset} on gpu ${!GPU_IDX}"
