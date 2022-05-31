f6d8928bd6849ce2a15a94ee18d01bebb3a6c20d
evonneng <evonneng@users.noreply.github.com>
2022-05-31 13:10:01 -0700

    removed group and prefixes for logging (#118)

--- radiance/nerf/trainer.py
+++ radiance/nerf/trainer.py
@@ -230,7 +230,7 @@ class Trainer:
             )
 
             if step != 0 and step % self.config.logging.steps_per_log == 0:
-                writer.put_dict(name="loss_dict", scalar_dict=loss_dict, step=step, group="Loss", prefix="train-")
+                writer.put_dict(name="Loss/train-loss_dict", scalar_dict=loss_dict, step=step)
                 # TODO: add the learning rates to tensorboard/logging
             if step != 0 and self.config.graph.steps_per_save and step % self.config.graph.steps_per_save == 0:
                 self.save_checkpoint(self.config.graph.model_dir, step)
