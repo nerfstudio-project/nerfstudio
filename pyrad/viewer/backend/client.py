3740ed92f70e8576697c2d6511f094e28d25c1cd
evonneng <evonne_ng@berkeley.edu>
2022-07-04 01:58:42 +0000

    rudimentary adding real-time imaging

--- pyrad/viewer/backend/client.py
+++ pyrad/viewer/backend/client.py
@@ -1,207 +1,199 @@
+"""Client class"""
 import argparse
 import asyncio
+import json
 import logging
 import math
-import traceback
-import numpy
-import cv2
+import os
+import ssl
 
-import socketio
+import aiohttp_cors
+import cv2
+import numpy
+from aiohttp import web
+from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+from aiortc.rtcrtpsender import RTCRtpSender
 from av import VideoFrame
 
-from aiortc import RTCIceCandidate, RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
-from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
-from aiortc.contrib.signaling import BYE
-from aiortc.sdp import candidate_from_sdp, candidate_to_sdp
+ROOT = os.path.dirname(__file__)
 
 
-class FlagVideoStreamTrack(VideoStreamTrack):
-    """
-    A video track that returns an animated flag.
-    """
-
+class SingleFrameStreamTrack(VideoStreamTrack):
     def __init__(self):
         super().__init__()  # don't forget this!
-        self.counter = 0
-        height, width = 480, 640
 
-        # generate flag
-        data_bgr = numpy.hstack(
-            [
-                self._create_rectangle(
-                    width=213, height=480, color=(255, 0, 0)
-                ),  # blue
-                self._create_rectangle(
-                    width=214, height=480, color=(255, 255, 255)
-                ),  # white
-                self._create_rectangle(width=213, height=480, color=(0, 0, 255)),  # red
-            ]
-        )
+        self.last_frame = None
+        self.frame_queue = asyncio.Queue(10)
 
-        # shrink and center it
-        M = numpy.float32([[0.5, 0, width / 4], [0, 0.5, height / 4]])
-        data_bgr = cv2.warpAffine(data_bgr, M, (width, height))
-
-        # compute animation
-        omega = 2 * math.pi / height
-        id_x = numpy.tile(numpy.array(range(width), dtype=numpy.float32), (height, 1))
-        id_y = numpy.tile(
-            numpy.array(range(height), dtype=numpy.float32), (width, 1)
-        ).transpose()
-
-        self.frames = []
-        for k in range(30):
-            phase = 2 * k * math.pi / 30
-            map_x = id_x + 10 * numpy.cos(omega * id_x + phase)
-            map_y = id_y + 10 * numpy.sin(omega * id_x + phase)
-            self.frames.append(
-                VideoFrame.from_ndarray(
-                    cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR), format="bgr24"
-                )
-            )
+    async def put_frame(self, frame):
+        await self.frame_queue.put(VideoFrame.from_ndarray(frame))
 
     async def recv(self):
+        if self.last_frame is None:
+            frame = await self.frame_queue.get()
+
+        else:
+            try:
+                frame = self.frame_queue.get_nowait()
+
+            except asyncio.QueueEmpty:
+                frame = self.last_frame
+
         pts, time_base = await self.next_timestamp()
 
-        frame = self.frames[self.counter % 30]
         frame.pts = pts
         frame.time_base = time_base
-        self.counter += 1
+        self.last_frame = frame
         return frame
 
-    def _create_rectangle(self, width, height, color):
-        data_bgr = numpy.zeros((height, width, 3), numpy.uint8)
-        data_bgr[:, :] = color
-        return data_bgr
+
+def _create_rectangle(width, height, color):
+    data_bgr = numpy.zeros((height, width, 3), numpy.uint8)
+    data_bgr[:, :] = color
+    return data_bgr
 
 
-def object_from_json(message):
-    print("got ", message)
-    if message["type"] in ["answer", "offer"]:
-        return RTCSessionDescription(**message)
-    elif message["type"] == "candidate" and message["candidate"]:
-        if len(message["candidate"]["candidate"]) != 0:
-            candidate = candidate_from_sdp(message["candidate"]["candidate"])
-            candidate.sdpMid = message["candidate"]["sdpMid"]
-            candidate.sdpMLineIndex = message["candidate"]["sdpMLineIndex"]
-            return candidate
-        else:
-            return None
-    elif message["type"] == "bye":
-        return BYE
+def _gen_test_image(k):
+    height, width = 480, 640
+
+    # generate flag
+    data_bgr = numpy.hstack(
+        [
+            _create_rectangle(width=213, height=480, color=(255, 0, 0)),  # blue
+            _create_rectangle(width=214, height=480, color=(255, 255, 255)),  # white
+            _create_rectangle(width=213, height=480, color=(0, 0, 255)),  # red
+        ]
+    )
+
+    # shrink and center it
+    M = numpy.float32([[0.5, 0, width / 4], [0, 0.5, height / 4]])
+    data_bgr = cv2.warpAffine(data_bgr, M, (width, height))
+
+    # compute animation
+    omega = 2 * math.pi / height
+    id_x = numpy.tile(numpy.array(range(width), dtype=numpy.float32), (height, 1))
+    id_y = numpy.tile(numpy.array(range(height), dtype=numpy.float32), (width, 1)).transpose()
+
+    phase = 2 * k * math.pi / 30
+    map_x = id_x + 10 * numpy.cos(omega * id_x + phase)
+    map_y = id_y + 10 * numpy.sin(omega * id_x + phase)
+    return cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR)
 
 
-def object_to_json(obj):
-    if isinstance(obj, RTCSessionDescription):
-        message = {"sdp": obj.sdp, "type": obj.type}
-    elif isinstance(obj, RTCIceCandidate):
-        message = {
-            "type": "candidate",
-            "candidate": {"candidate": candidate_to_sdp(obj), "sdpMid": obj.sdpMid, "sdpMLineIndex": obj.sdpMLineIndex},
-        }
-    else:
-        assert obj is BYE
-        message = {"type": "bye"}
-
-    return message
-
-
-class Ready:
-    pass
-
-
-class Signaling:
-    def __init__(self):
-        sio = socketio.AsyncClient()
-        self.queue = asyncio.Queue()
-
-        @sio.on("data")
-        async def on_data(data):
-            obj = object_from_json(data)
-            if obj is not None:
-                await self.queue.put(obj)
-
-        @sio.on("ready")
-        async def on_ready():
-            await self.queue.put(Ready())
-
-        self.sio = sio
-
-    async def connect(self, host, port):
-        await self.sio.connect("http://{}:{}".format(host, port))
-
-    async def send(self, data):
-        await self.sio.emit("data", object_to_json(data))
-
-    async def receive(self):
-        return await self.queue.get()
-
-    async def close(self):
-        pass
-
-
-async def run(host, port, pc, video_track, recorder, signaling):
-    await signaling.connect(host, port)
-
-    def add_tracks():
-        pc.addTrack(video_track)
-
+async def gen_image_frames(video_track):
+    k = 0
     while True:
-        obj = await signaling.receive()
+        if k > 500:
+            k = 0
+        img = _gen_test_image(k)
+        k += 1
 
-        if isinstance(obj, Ready):
-            print("ready")
-            add_tracks()
-            await pc.setLocalDescription(await pc.createOffer())
-            await signaling.send(pc.localDescription)
+        await video_track.put_frame(img)
 
-        elif isinstance(obj, RTCSessionDescription):
-            await pc.setRemoteDescription(obj)
-            await recorder.start()
 
-            if obj.type == "offer":
-                print("received offer")
-                # send answer
-                add_tracks()
-                answer = await pc.createAnswer()
-                await pc.setLocalDescription(answer)
-                await signaling.send(pc.localDescription)
+def force_codec(pc, sender, forced_codec):
+    kind = forced_codec.split("/")[0]
+    codecs = RTCRtpSender.getCapabilities(kind).codecs
+    transceiver = next(t for t in pc.getTransceivers() if t.sender == sender)
+    transceiver.setCodecPreferences([codec for codec in codecs if codec.mimeType == forced_codec])
 
-        elif isinstance(obj, RTCIceCandidate):
-            await pc.addIceCandidate(obj)
 
-        elif obj is BYE:
-            print("Exiting")
-            break
+async def offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        print("Connection state is %s" % pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    video = SingleFrameStreamTrack()
+    loop = asyncio.get_event_loop()
+    try:
+        asyncio.run_coroutine_threadsafe(gen_image_frames(video), loop)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        pc.close()
+
+    if video:
+        video_sender = pc.addTrack(video)
+        if args.video_codec:
+            force_codec(pc, video_sender, args.video_codec)
+        elif args.play_without_decoding:
+            raise Exception("You must specify the video codec using --video-codec")
+
+    await pc.setRemoteDescription(offer)
+
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps({"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}),
+    )
+
+
+pcs = set()
+
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--sighost", default="localhost")
-    parser.add_argument("--sigport", default=8052)
+    parser = argparse.ArgumentParser(description="WebRTC webcam demo")
+    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    parser.add_argument(
+        "--play-without-decoding",
+        help=(
+            "Read the media without decoding it (experimental). "
+            "For now it only works with an MPEGTS container with only H.264 video."
+        ),
+        action="store_true",
+    )
+    parser.add_argument("--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)")
+    parser.add_argument("--port", type=int, default=8052, help="Port for HTTP server (default: 8080)")
+    parser.add_argument("--verbose", "-v", action="count")
+    parser.add_argument("--audio-codec", help="Force a specific audio codec (e.g. audio/opus)")
+    parser.add_argument("--video-codec", help="Force a specific video codec (e.g. video/H264)")
 
     args = parser.parse_args()
 
-    video_track = FlagVideoStreamTrack()
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
-    recorder = MediaBlackhole()
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None  # pylint: disable=invalid-name
 
-    pc = RTCPeerConnection()
-    signaling = Signaling()
+    app = web.Application()
+    app.router.add_route("POST", "/offer", offer)
+    app.on_shutdown.append(on_shutdown)
+    cors = aiohttp_cors.setup(
+        app,
+        defaults={
+            "*": aiohttp_cors.ResourceOptions(
+                allow_credentials=True,
+                expose_headers="*",
+                allow_headers="*",
+            )
+        },
+    )
+    for route in list(app.router.routes()):
+        cors.add(route)
 
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(run(args.sighost, args.sigport, pc, video_track, recorder, signaling))
-
-    except KeyboardInterrupt:
-        pass
-
-    finally:
-        print("stopping recorder")
-        loop.run_until_complete(recorder.stop())
-        print("closing signaling client")
-        loop.run_until_complete(signaling.close())
-        print("closing peer connection")
-        loop.run_until_complete(pc.close())
-        print("done")
+    web.run_app(app, host=args.host, port=args.port, ssl_context=ssl_context)
