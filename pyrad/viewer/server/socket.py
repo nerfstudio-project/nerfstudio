45d90eed7d77438b60c76e6e5da4516bb1771d1a
Ethan Weber <eweb0124@gmail.com>
2022-07-07 06:53:57 +0000

    black

--- pyrad/viewer/server/socket.py
+++ pyrad/viewer/server/socket.py
@@ -3,7 +3,7 @@
 # zmqimage.py -- classes to send, receive and display cv2 images via zmq
 #     based on serialization in pyzmq docs and pyzmq/examples/serialization
 
-'''
+"""
 PURPOSE:
     These classes allow a headless (no display) computer running OpenCV code
     to display OpenCV images on another computer with a display.
@@ -47,15 +47,16 @@ CAVEATS:
     defaults in the classes but may be overridden.
 AUTHOR:
     Jeff Bass, https://github.com/jeffbass, jeff@yin-yang-ranch.com
-'''
+"""
 
 import zmq
 import numpy as np
 import cv2
 
+
 class SerializingSocket(zmq.Socket):
     """A class with some extra serialization methods
-    
+
     send_array sends numpy arrays with metadata necessary
     for reconstructing the array on the other side (dtype,shape).
     Also sends array name for display with cv2.show(image).
@@ -63,39 +64,41 @@ class SerializingSocket(zmq.Socket):
     and reconstructs the array with the correct shape and array name.
     """
 
-    def send_array(self, A, arrayname="NoName",flags=0, copy=True, track=False):
+    def send_array(self, A, arrayname="NoName", flags=0, copy=True, track=False):
         """send a numpy array with metadata and array name"""
         md = dict(
-            arrayname = arrayname,
-            dtype = str(A.dtype),
-            shape = A.shape,
+            arrayname=arrayname,
+            dtype=str(A.dtype),
+            shape=A.shape,
         )
-        self.send_json(md, flags|zmq.SNDMORE)
+        self.send_json(md, flags | zmq.SNDMORE)
         return self.send(A, flags, copy=copy, track=track)
 
     def recv_array(self, flags=0, copy=True, track=False):
         """recv a numpy array, including arrayname, dtype and shape"""
         md = self.recv_json(flags=flags)
         msg = self.recv(flags=flags, copy=copy, track=track)
-        A = np.frombuffer(msg, dtype=md['dtype'])
-        return (md['arrayname'], A.reshape(md['shape']))
+        A = np.frombuffer(msg, dtype=md["dtype"])
+        return (md["arrayname"], A.reshape(md["shape"]))
+
 
 class SerializingContext(zmq.Context):
     _socket_class = SerializingSocket
 
-class zmqConnect():
-    '''A class that opens a zmq REQ socket on the headless computer
-    '''
+
+class zmqConnect:
+    """A class that opens a zmq REQ socket on the headless computer"""
+
     def __init__(self, connect_to="tcp://jeff-mac:5555"):
-        '''initialize zmq socket for sending images to display on remote computer'''
-        '''connect_to is the tcp address:port of the display computer'''
+        """initialize zmq socket for sending images to display on remote computer"""
+        """connect_to is the tcp address:port of the display computer"""
         self.zmq_context = SerializingContext()
         self.zmq_socket = self.zmq_context.socket(zmq.REQ)
         self.zmq_socket.connect(connect_to)
- 
+
     def imshow(self, arrayname, array):
-        '''send image to display on remote server'''
-        if array.flags['C_CONTIGUOUS']:
+        """send image to display on remote server"""
+        if array.flags["C_CONTIGUOUS"]:
             # if array is already contiguous in memory just send it
             self.zmq_socket.send_array(array, arrayname, copy=False)
         else:
@@ -104,17 +107,18 @@ class zmqConnect():
             self.zmq_socket.send_array(array, arrayname, copy=False)
         message = self.zmq_socket.recv()
 
-class zmqImageShowServer():
-    '''A class that opens a zmq REP socket on the display computer to receive images
-    '''
+
+class zmqImageShowServer:
+    """A class that opens a zmq REP socket on the display computer to receive images"""
+
     def __init__(self, open_port="tcp://*:5555"):
-        '''initialize zmq socket on viewing computer that will display images'''
+        """initialize zmq socket on viewing computer that will display images"""
         self.zmq_context = SerializingContext()
         self.zmq_socket = self.zmq_context.socket(zmq.REP)
         self.zmq_socket.bind(open_port)
 
     def imshow(self, copy=False):
-        '''receive and show image on viewing computer display'''
+        """receive and show image on viewing computer display"""
         arrayname, image = self.zmq_socket.recv_array(copy=False)
         # print "Received Array Named: ", arrayname
         # print "Array size: ", image.shape
