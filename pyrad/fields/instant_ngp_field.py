42ece500655b1998f4a7a62cfc15ab3c2b1116e6
Matt Tancik <tancik@berkeley.edu>
2022-07-18 15:32:20 -0700

    Remove extra var

--- pyrad/fields/instant_ngp_field.py
+++ pyrad/fields/instant_ngp_field.py
@@ -63,18 +63,6 @@ class TCNNInstantNGPField(Field):
         # TODO: set this properly based on the aabb
         per_level_scale = 1.4472692012786865
 
-        self.position_encoding = tcnn.Encoding(
-            n_input_dims=3,
-            encoding_config={
-                "otype": "HashGrid",
-                "n_levels": 16,
-                "n_features_per_level": 2,
-                "log2_hashmap_size": 19,
-                "base_resolution": 16,
-                "per_level_scale": per_level_scale,
-            },
-        )
-
         self.direction_encoding = tcnn.Encoding(
             n_input_dims=3,
             encoding_config={
