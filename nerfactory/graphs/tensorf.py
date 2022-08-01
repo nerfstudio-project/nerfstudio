260d8a2181ccb1049ffa20ab40f3deb683905c7b
terrance.3512@gmail.com <terrance.3512@gmail.com>
2022-08-01 14:43:29 -0700

    initial working version of tensorf

--- nerfactory/graphs/tensorf.py
+++ nerfactory/graphs/tensorf.py
@@ -49,10 +49,18 @@ class TensoRFGraph(Graph):
         self,
         intrinsics: torch.Tensor = None,
         camera_to_world: torch.Tensor = None,
+        near_plane: float = 2.0,
+        far_plane: float = 6.0,
+        num_coarse_samples: int = 64,
+        num_importance_samples: int = 128,
         enable_density_field: bool = False,
         **kwargs,
     ) -> None:
+        self.near_plane = near_plane
+        self.far_plane = far_plane
         self.field = None
+        self.num_coarse_samples = num_coarse_samples
+        self.num_importance_samples = num_importance_samples
         super().__init__(
             intrinsics=intrinsics, camera_to_world=camera_to_world, enable_density_field=enable_density_field, **kwargs
         )
@@ -102,44 +110,74 @@ class TensoRFGraph(Graph):
 
     def get_param_groups(self) -> Dict[str, List[Parameter]]:
         param_groups = {}
+        if self.field is None:
+            raise ValueError("populate_fields() must be called before get_param_groups")
         param_groups["fields"] = list(self.field.parameters())
         param_groups["position_encoding"] = list(self.field.position_encoding.parameters())
         param_groups["direction_encoding"] = list(self.field.direction_encoding.parameters())
         return param_groups
 
     def get_outputs(self, ray_bundle: RayBundle):
+
+        if self.field is None:
+            raise ValueError("populate_fields() must be called before get_outputs")
+
         # uniform sampling
         ray_samples_uniform = self.sampler_uniform(ray_bundle)
 
         # coarse field:
-        field_outputs_coarse = self.field_coarse.forward(ray_samples_uniform)
-        weights_coarse = ray_samples_uniform.get_weights(field_outputs_coarse[FieldHeadNames.DENSITY])
-        rgb_coarse = self.renderer_rgb(
+        field_outputs_coarse = self.field.forward(ray_samples_uniform)
+        weights = ray_samples_uniform.get_weights(field_outputs_coarse[FieldHeadNames.DENSITY])
+        rgb = self.renderer_rgb(
             rgb=field_outputs_coarse[FieldHeadNames.RGB],
-            weights=weights_coarse,
+            weights=weights,
         )
-        accumulation_coarse = self.renderer_accumulation(weights_coarse)
-        depth_coarse = self.renderer_depth(weights_coarse, ray_samples_uniform)
+        accumulation = self.renderer_accumulation(weights)
+        depth = self.renderer_depth(weights, ray_samples_uniform)
 
-        # pdf sampling
-        ray_samples_pdf = self.sampler_pdf(ray_bundle, ray_samples_uniform, weights_coarse)
-
-        # fine field:
-        field_outputs_fine = self.field_fine.forward(ray_samples_pdf)
-        weights_fine = ray_samples_pdf.get_weights(field_outputs_fine[FieldHeadNames.DENSITY])
-        rgb_fine = self.renderer_rgb(
-            rgb=field_outputs_fine[FieldHeadNames.RGB],
-            weights=weights_fine,
-        )
-        accumulation_fine = self.renderer_accumulation(weights_fine)
-        depth_fine = self.renderer_depth(weights_fine, ray_samples_pdf)
-
-        outputs = {
-            "rgb_coarse": rgb_coarse,
-            "rgb_fine": rgb_fine,
-            "accumulation_coarse": accumulation_coarse,
-            "accumulation_fine": accumulation_fine,
-            "depth_coarse": depth_coarse,
-            "depth_fine": depth_fine,
-        }
+        outputs = {"rgb": rgb, "accumulation": accumulation, "depth": depth}
         return outputs
+
+    def get_loss_dict(self, outputs, batch, metrics_dict, loss_coefficients) -> Dict[str, torch.Tensor]:
+        # Scaling metrics by coefficients to create the losses.
+        device = outputs["rgb"].device
+        image = batch["image"].to(device)
+
+        rgb_loss = self.rgb_loss(image, outputs["rgb"])
+
+        loss_dict = {"rgb_loss": rgb_loss}
+        loss_dict = misc.scale_dict(loss_dict, loss_coefficients)
+        return loss_dict
+
+    def log_test_image_outputs(self, image_idx, step, batch, outputs):
+        image = batch["image"]
+        rgb = outputs["rgb"]
+        acc = visualization.apply_colormap(outputs["accumulation"])
+        depth = visualization.apply_depth_colormap(
+            outputs["depth"],
+            accumulation=outputs["accumulation"],
+            near_plane=self.near_plane,
+            far_plane=self.far_plane,
+        )
+
+        combined_rgb = torch.cat([image, rgb], dim=1)
+
+        writer.put_image(name=f"img/image_idx_{image_idx}", image=combined_rgb, step=step)
+        writer.put_image(name=f"accumulation/image_idx_{image_idx}", image=acc, step=step)
+        writer.put_image(name=f"depth/image_idx_{image_idx}", image=depth, step=step)
+
+        # Switch images from [H, W, C] to [1, C, H, W] for metrics computations
+        image = torch.moveaxis(image, -1, 0)[None, ...]
+        rgb = torch.moveaxis(rgb, -1, 0)[None, ...]
+
+        psnr = self.psnr(image, rgb)
+        ssim = self.ssim(image, rgb)
+        lpips = self.lpips(image, rgb)
+
+        writer.put_scalar(name=f"psnr/val_{image_idx}", scalar=float(psnr), step=step)
+        writer.put_scalar(name=f"ssim/val_{image_idx}", scalar=float(ssim), step=step)  # type: ignore
+        writer.put_scalar(name=f"lpips/val_{image_idx}", scalar=float(lpips), step=step)
+
+        writer.put_scalar(name=writer.EventName.CURR_TEST_PSNR, scalar=float(psnr), step=step)
+
+        return psnr.item()
