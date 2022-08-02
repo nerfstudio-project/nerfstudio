9f069f68f81fe1546bff206846e80602aa677002
Matt Tancik <tancik@berkeley.edu>
2022-08-02 15:45:12 -0700

    Fix linting

--- nerfactory/viewer/app/src/App.jsx
+++ nerfactory/viewer/app/src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css';
 
 import React from 'react';
-import { RenderControls } from './modules/ConfigPanel/ConfigPanel';
 import { Leva } from 'leva';
+import { RenderControls } from './modules/ConfigPanel/ConfigPanel';
 
+import LevaTheme from './modules/ConfigPanel/leva_theme.json';
 import SetupScene from './modules/Scene/Scene';
 import ViewerWindow from './modules/ViewerWindow/ViewerWindow';
 
 export default function App() {
   const scene = SetupScene();
-  const theme = require('./modules/ConfigPanel/leva_theme.json');
 
   return (
     <div className="App">
-      <Leva theme={theme} titleBar={false} />
+      <Leva theme={LevaTheme} titleBar={false} />
       <RenderControls />
       <ViewerWindow scene={scene} />
     </div>
