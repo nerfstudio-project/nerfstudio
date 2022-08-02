f7fa4e15ee41a7e107df65e7d84843766be8b89d
Matt Tancik <tancik@berkeley.edu>
2022-08-02 15:34:58 -0700

    Add leva theme

--- nerfactory/viewer/app/src/App.jsx
+++ nerfactory/viewer/app/src/App.jsx
@@ -2,15 +2,18 @@ import './App.css';
 
 import React from 'react';
 import { RenderControls } from './modules/ConfigPanel/ConfigPanel';
+import { Leva } from 'leva';
 
 import SetupScene from './modules/Scene/Scene';
 import ViewerWindow from './modules/ViewerWindow/ViewerWindow';
 
 export default function App() {
   const scene = SetupScene();
+  const theme = require('./modules/ConfigPanel/leva_theme.json');
 
   return (
     <div className="App">
+      <Leva theme={theme} titleBar={false} />
       <RenderControls />
       <ViewerWindow scene={scene} />
     </div>
