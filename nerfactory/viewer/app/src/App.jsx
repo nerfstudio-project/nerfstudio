dfe13334e72a8a3b02cefbcfc31981b6f8144dea
Matthew Tancik <tancik@berkeley.edu>
2022-08-02 15:49:21 -0700

    Add leva theme (#245)
    * Add leva theme
    
    * Fix linting

--- nerfactory/viewer/app/src/App.jsx
+++ nerfactory/viewer/app/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
 
 import React from 'react';
+import { Leva } from 'leva';
 import { RenderControls } from './modules/ConfigPanel/ConfigPanel';
 
+import LevaTheme from './modules/ConfigPanel/leva_theme.json';
 import SetupScene from './modules/Scene/Scene';
 import ViewerWindow from './modules/ViewerWindow/ViewerWindow';
 
@@ -11,6 +13,7 @@ export default function App() {
 
   return (
     <div className="App">
+      <Leva theme={LevaTheme} titleBar={false} />
       <RenderControls />
       <ViewerWindow scene={scene} />
     </div>
