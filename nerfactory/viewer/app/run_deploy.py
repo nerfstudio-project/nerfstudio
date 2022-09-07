619731c95cbf21125586f7caf5e9074270ffd435
Ethan Weber <eweb0124@gmail.com>
2022-09-07 05:10:10 +0000

    lazy commit

--- nerfactory/viewer/app/run_deploy.py
+++ nerfactory/viewer/app/run_deploy.py
@@ -35,8 +35,8 @@ def main(
     ssh_key_string: str = "",
     ssh_key_filename: str = " ~/.ssh/github_actions_user_key_filename",
     hostname_or_ip_address: str = "34.102.68.79",
-    local_folder: str = "temp",
-    remote_folder: str = "temp",
+    local_folder: str = "/home/eweb0124/build",
+    remote_folder: str = "/home/eweb0124/viewer",
     host: str = "viewer_deploy_host",
     user: str = "eweb0124",
 ):
@@ -95,8 +95,9 @@ def main(
     target_path = "/path/to/folder/{version_new}"
     symlink_path = "/path/to/folder/latest"
 
-    os.system("""ssh github_action_remote_machine 'rm -rf /home/eweb0124/build'""")
-    os.system("""ssh github_action_remote_machine 'mkdir /home/eweb0124/viewer/branch/temp'""")
+    os.system(f"""ssh {host} 'rm -rf /home/eweb0124/build'""")
+    os.system(f"""scp -r {local_folder} {host}:/home/eweb0124/""")
+    os.system(f"""ssh {host} 'mkdir {remote_folder}/branch/temp'""")
 
     print("target path")
     print(target_path)
