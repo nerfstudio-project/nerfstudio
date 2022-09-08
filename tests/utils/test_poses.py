70a8109a824a5e0f3316cb99bd0468ab3420503a
Alexander Kristoffersen <akristoffersen@berkeley.edu>
2022-09-08 14:09:06 -0700

    tests done.

--- tests/utils/test_poses.py
+++ tests/utils/test_poses.py
@@ -21,6 +21,42 @@ def test_to4x4():
     )
 
 
+def test_multiply():
+    """Test 3D pose multiplication."""
+    pose = torch.tensor(
+        [
+            [
+                [1.0, 0.0, 0.0, 1.0],
+                [0.0, 1.0, 0.0, 2.0],
+                [0.0, 0.0, 1.0, 3.0],
+            ]
+        ]
+    )
+    translation_pose = poses.multiply(pose, pose)
+    assert translation_pose.shape == pose.shape
+    assert torch.equal(translation_pose[..., :, 3], torch.tensor([[2.0, 4.0, 6.0]]))
+
+    pose_a = pose.clone()
+    pose_a[:, :3, :3] = torch.tensor(
+        [
+            [1.0, 0.0, 0.0],
+            [0.0, 0.0, -1.0],
+            [0.0, 1.0, 0.0],
+        ]
+    )
+    pose_b = pose.clone()
+    pose_b[:, :3, :3] = torch.tensor(
+        [
+            [0.0, -1.0, 0.0],
+            [0.0, 0.0, -1.0],
+            [1.0, 0.0, 0.0],
+        ]
+    )
+
+    translation_rotation_pose = poses.multiply(pose_a, pose_b)
+    assert torch.allclose(translation_rotation_pose, (poses.to4x4(pose_a) @ poses.to4x4(pose_b))[:, :3, :4])
+
+
 def test_inverse():
     """Test 3D pose inversion."""
 
@@ -43,8 +79,3 @@ def test_inverse():
         poses.multiply(pose, pose_inv),
         unit_pose,
     )
-
-
-def test_multiply():
-    """Test 3D pose multiplication."""
-    pass
