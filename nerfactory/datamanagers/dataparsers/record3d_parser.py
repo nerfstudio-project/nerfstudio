fix coordinate system rotation.

nerfactory/datamanagers/dataparsers/record3d_parser.py
@@ -91,16 +91,9 @@ class Record3D(DataParser):
         # convert to Tensors
         poses = torch.from_numpy(poses[:, :3, :4])
 
-        # convert OpenGL to instantNGP coordinate system
-        rotation_matrix = torch.zeros_like(poses)
-        rotation_matrix[:, :3, :3] = torch.tensor(
-            [
-                [0.0, 0.0, 1.0],
-                [-1.0, 0.0, 0.0],
-                [0.0, -1.0, 0.0],
-            ]
-        )
-        poses = pose_utils.multiply(rotation_matrix, poses)
+        # convert OpenGL to OpenCV coordinate system
+        poses = poses @ torch.diag(torch.tensor([1.0, -1.0, -1.0, 1.0]))
+
         poses = camera_utils.auto_orient_poses(pose_utils.to4x4(poses), method="pca")[:, :3, :4]
 
         # Centering poses