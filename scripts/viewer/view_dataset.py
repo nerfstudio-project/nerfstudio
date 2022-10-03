d38e9c1f24433d154c9f918cc595cb22b7ea7aac
Brent Yi <yibrenth@gmail.com>
2022-10-03 01:14:03 -0700

    Fix view_dataset.py

--- scripts/viewer/view_dataset.py
+++ scripts/viewer/view_dataset.py
@@ -11,9 +11,9 @@ from pathlib import Path
 import dcargs
 import torch
 
-from nerfstudio.configs import base as cfg
-from nerfstudio.data.base import AnnotatedDataParserUnion
-from nerfstudio.data.datasets import InputDataset
+from nerfstudio.configs import model_configs as cfg
+from nerfstudio.data.datamanagers import AnnotatedDataParserUnion
+from nerfstudio.data.utils.datasets import InputDataset
 from nerfstudio.viewer.server import viewer_utils
 
 logging.basicConfig(format="[%(filename)s:%(lineno)d] %(message)s", level=logging.INFO)
@@ -27,12 +27,15 @@ torch.backends.cudnn.benchmark = True  # type: ignore
 def main(
     dataparser: AnnotatedDataParserUnion,
     viewer: cfg.ViewerConfig,
-    log_base_dir: Path = Path("/tmp/nerfstudio_viewer_logs"),
+    log_base_dir: Path = Path("/tmp/nerfactory_viewer_logs"),
 ) -> None:
     """Main function."""
-    viewer_state = viewer_utils.ViewerState(config.viewer)
-    datamanager = config.pipeline.datamanager.setup()
-    viewer_state.init_scene(dataset=datamanager.train_input_dataset, start_train=False)
+    viewer_state = viewer_utils.ViewerState(
+        viewer,
+        log_filename=log_base_dir / viewer.relative_log_filename,
+    )
+    dataset = InputDataset(dataparser.setup().get_dataset_inputs(split="train"))
+    viewer_state.init_scene(dataset=dataset, start_train=False)
     logging.info("Please refresh and load page at: %s", viewer_state.viewer_url)
     time.sleep(30)  # allowing time to refresh page
 
