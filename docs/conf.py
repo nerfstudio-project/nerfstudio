b7495179c7b3822c1954478bce6e0e718a6666c6
Matt Tancik <tancik@berkeley.edu>
2022-10-05 02:10:44 -0700

    version 0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2022, nerfstudio Team"
 author = "nerfstudio Team"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.0"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
