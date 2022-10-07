745edbc2c871831981e1f5b90916d17eb1ac00ef
Ethan Weber <eweb0124@gmail.com>
2022-10-07 15:51:26 -0700

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2022, nerfstudio Team"
 author = "nerfstudio Team"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
