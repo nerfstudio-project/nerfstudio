95a269fa8ffdc4f1020e617c9418b3f2c00c1fbe
Jake Austin <jake-austin@berkeley.edu>
2022-10-20 08:23:29 +0000

    Fixing linting for tests

--- tests/cameras/test_cameras.py
+++ tests/cameras/test_cameras.py
@@ -9,104 +9,104 @@ import torch
 from nerfstudio.cameras.cameras import Cameras
 from nerfstudio.cameras.rays import RayBundle
 
-batch_size = 2
-h_w = 800
-fx_y = 10.0
-cx_y = h_w / 2.0
+BATCH_SIZE = 2
+H_W = 800
+FX_Y = 10.0
+CX_Y = H_W / 2.0
 # pylint: disable=unnecessary-comprehension
-c2w_flat = torch.eye(4)[:3, :]
-camera_to_worlds = [
-    c2w_flat,
-    torch.stack([c2w_flat] * batch_size),
-    torch.stack([torch.stack([c2w_flat] * batch_size)] * batch_size),
+C2W_FLAT = torch.eye(4)[:3, :]
+CAMERA_TO_WORLDS = [
+    C2W_FLAT,
+    torch.stack([C2W_FLAT] * BATCH_SIZE),
+    torch.stack([torch.stack([C2W_FLAT] * BATCH_SIZE)] * BATCH_SIZE),
 ]
-fx_ys = [
-    fx_y,
-    torch.tensor(1).float() * fx_y,
-    torch.ones(batch_size) * fx_y,
-    torch.ones(batch_size, 1) * fx_y,
-    torch.ones((batch_size, batch_size)) * fx_y,
+FX_YS = [
+    FX_Y,
+    torch.tensor(1).float() * FX_Y,
+    torch.ones(BATCH_SIZE) * FX_Y,
+    torch.ones(BATCH_SIZE, 1) * FX_Y,
+    torch.ones((BATCH_SIZE, BATCH_SIZE)) * FX_Y,
 ]
-h_ws = [
+H_WS = [
     None,
-    h_w,
-    torch.tensor(1) * h_w,
-    torch.ones(batch_size).int() * h_w,
-    torch.ones(batch_size, 1).int() * h_w,
-    torch.ones((batch_size, batch_size)).int() * h_w,
+    H_W,
+    torch.tensor(1) * H_W,
+    torch.ones(BATCH_SIZE).int() * H_W,
+    torch.ones(BATCH_SIZE, 1).int() * H_W,
+    torch.ones((BATCH_SIZE, BATCH_SIZE)).int() * H_W,
 ]
-cx_ys = [
-    cx_y,
-    torch.tensor(1).float() * cx_y,
-    torch.ones(batch_size) * cx_y,
-    torch.ones(batch_size, 1) * cx_y,
-    torch.ones((batch_size, batch_size)) * cx_y,
+CX_YS = [
+    CX_Y,
+    torch.tensor(1).float() * CX_Y,
+    torch.ones(BATCH_SIZE) * CX_Y,
+    torch.ones(BATCH_SIZE, 1) * CX_Y,
+    torch.ones((BATCH_SIZE, BATCH_SIZE)) * CX_Y,
 ]
-distortion_params = [None, torch.zeros(6), torch.zeros((batch_size, 6)), torch.zeros((batch_size, batch_size, 6))]
+DISTORTION_PARAMS = [None, torch.zeros(6), torch.zeros((BATCH_SIZE, 6)), torch.zeros((BATCH_SIZE, BATCH_SIZE, 6))]
 camera_types = [
     1,
     torch.tensor(1),
-    torch.ones(batch_size).int(),
-    torch.ones(batch_size, 1).int(),
-    torch.ones((batch_size, batch_size)).int(),
+    torch.ones(BATCH_SIZE).int(),
+    torch.ones(BATCH_SIZE, 1).int(),
+    torch.ones((BATCH_SIZE, BATCH_SIZE)).int(),
 ]
-c = Cameras(
-    camera_to_worlds[1],
-    fx_ys[0],
-    fx_ys[0],
-    cx_ys[0],
-    cx_ys[0],
-    h_ws[0],
-    h_ws[0],
-    distortion_params[0],
+C = Cameras(
+    CAMERA_TO_WORLDS[1],
+    FX_YS[0],
+    FX_YS[0],
+    CX_YS[0],
+    CX_YS[0],
+    H_WS[0],
+    H_WS[0],
+    DISTORTION_PARAMS[0],
     camera_types[0],
 )
 
-c0 = Cameras(
-    camera_to_worlds[0],
-    fx_ys[0],
-    fx_ys[0],
-    cx_ys[0],
-    cx_ys[0],
-    h_ws[0],
-    h_ws[0],
-    distortion_params[0],
+C0 = Cameras(
+    CAMERA_TO_WORLDS[0],
+    FX_YS[0],
+    FX_YS[0],
+    CX_YS[0],
+    CX_YS[0],
+    H_WS[0],
+    H_WS[0],
+    DISTORTION_PARAMS[0],
     camera_types[0],
 )
 
-c1 = Cameras(
-    camera_to_worlds[1],
-    fx_ys[0],
-    fx_ys[0],
-    cx_ys[0],
-    cx_ys[0],
-    h_ws[0],
-    h_ws[0],
-    distortion_params[0],
+C1 = Cameras(
+    CAMERA_TO_WORLDS[1],
+    FX_YS[0],
+    FX_YS[0],
+    CX_YS[0],
+    CX_YS[0],
+    H_WS[0],
+    H_WS[0],
+    DISTORTION_PARAMS[0],
     camera_types[0],
 )
 
-c2 = Cameras(
-    camera_to_worlds[2],
-    fx_ys[0],
-    fx_ys[0],
-    cx_ys[0],
-    cx_ys[0],
-    h_ws[0],
-    h_ws[0],
-    distortion_params[0],
+C2 = Cameras(
+    CAMERA_TO_WORLDS[2],
+    FX_YS[0],
+    FX_YS[0],
+    CX_YS[0],
+    CX_YS[0],
+    H_WS[0],
+    H_WS[0],
+    DISTORTION_PARAMS[0],
     camera_types[0],
 )
 
-c2_dist = Cameras(
-    camera_to_worlds[2],
-    fx_ys[0],
-    fx_ys[0],
-    cx_ys[0],
-    cx_ys[0],
-    h_ws[0],
-    h_ws[0],
-    distortion_params[1],
+C2_DIST = Cameras(
+    CAMERA_TO_WORLDS[2],
+    FX_YS[0],
+    FX_YS[0],
+    CX_YS[0],
+    CX_YS[0],
+    H_WS[0],
+    H_WS[0],
+    DISTORTION_PARAMS[1],
     camera_types[0],
 )
 
@@ -127,41 +127,41 @@ def test_pinhole_camera():
 
 def test_camera_as_tensordataclass():
     """Test that the camera class move to Tensordataclass works."""
-    _ = c2_dist[torch.tensor([0]), torch.tensor([0])]
+    _ = C2_DIST[torch.tensor([0]), torch.tensor([0])]
 
-    assert c0.shape == ()
-    assert c1[...].shape == torch.Size([batch_size])
+    assert C0.shape == ()
+    assert C1[...].shape == torch.Size([BATCH_SIZE])
 
-    assert _check_cam_shapes(c0, ())
-    assert _check_cam_shapes(c1, (2,))
-    assert _check_cam_shapes(c2_dist, (2, 2))
+    assert _check_cam_shapes(C0, ())
+    assert _check_cam_shapes(C1, (2,))
+    assert _check_cam_shapes(C2_DIST, (2, 2))
 
-    assert c0.generate_rays(0).shape == (h_w, h_w)
-    assert c0.generate_rays(0, coords=torch.ones(10, 2)).shape == (10,)
-    c1.generate_rays(0)
-    c1.generate_rays(torch.tensor([0, 1]).unsqueeze(-1))
+    assert C0.generate_rays(0).shape == (H_W, H_W)
+    assert C0.generate_rays(0, coords=torch.ones(10, 2)).shape == (10,)
+    C1.generate_rays(0)
+    C1.generate_rays(torch.tensor([0, 1]).unsqueeze(-1))
 
     # Make sure rays generated are same when distortion params are identity (all zeros) and None
-    assert c2.shape == c2_dist.shape
+    assert C2.shape == C2_DIST.shape
 
-    c2_rays = c2.generate_rays(torch.tensor([0, 0]))
-    c_dist_rays = c2_dist.generate_rays(torch.tensor([0, 0]))
+    c2_rays = C2.generate_rays(torch.tensor([0, 0]))
+    c_dist_rays = C2_DIST.generate_rays(torch.tensor([0, 0]))
     assert _check_dataclass_allclose(c2_rays, c_dist_rays)
-    assert c2_rays.shape == (h_w, h_w)
-    assert c_dist_rays.shape == (h_w, h_w)
+    assert c2_rays.shape == (H_W, H_W)
+    assert c_dist_rays.shape == (H_W, H_W)
 
     camera_indices = torch.tensor([[0, 0]])
-    assert c2.generate_rays(camera_indices).shape == (h_w, h_w, 1)
+    assert C2.generate_rays(camera_indices).shape == (H_W, H_W, 1)
 
     for args in product(
-        camera_to_worlds,
-        fx_ys,
-        fx_ys[:1],
-        cx_ys,
-        cx_ys[:1],
-        h_ws,
-        h_ws[:1],
-        distortion_params[:-1],
+        CAMERA_TO_WORLDS,
+        FX_YS,
+        FX_YS[:1],
+        CX_YS,
+        CX_YS[:1],
+        H_WS,
+        H_WS[:1],
+        DISTORTION_PARAMS[:-1],
         camera_types[:-1],
     ):
         _ = Cameras(*args)
@@ -220,12 +220,12 @@ def check_generate_rays_shape():
         ),  # [0]th camera and all pixels since coords is none but we still have 2 extra batch dims of [11, 5] in coords
     ]
     for camera_indices, coords, output_size in combos:
-        shape = c0.generate_rays(camera_indices=camera_indices, coords=coords).shape
+        shape = C0.generate_rays(camera_indices=camera_indices, coords=coords).shape
         assert shape == output_size
 
-    assert c1.shape == (2,)
+    assert C1.shape == (2,)
     for camera_indices, coords, output_size in combos:
-        shape = c1.generate_rays(camera_indices=camera_indices, coords=coords).shape
+        shape = C1.generate_rays(camera_indices=camera_indices, coords=coords).shape
         assert shape == output_size
 
     # camera_indices can't be an int anymore since our cameras object have 2 batch dimensions
@@ -240,7 +240,7 @@ def check_generate_rays_shape():
         (torch.zeros(11, 5, 2), None, (800, 800, 11, 5)),
     ]
     for camera_indices, coords, output_size in combos:
-        shape = c2.generate_rays(camera_indices=camera_indices, coords=coords).shape
+        shape = C2.generate_rays(camera_indices=camera_indices, coords=coords).shape
         assert shape == output_size
 
 
@@ -251,19 +251,19 @@ def _check_dataclass_allclose(ipt, other):
     return True
 
 
-def _check_cam_shapes(c: Cameras, batch_size):
-    if batch_size:
-        assert len(c) == batch_size[0]
-    assert c.shape == (*batch_size,)
-    assert c.camera_to_worlds.shape == (*batch_size, 3, 4)
-    assert c.fx.shape == (*batch_size, 1)
-    assert c.fy.shape == (*batch_size, 1)
-    assert c.cx.shape == (*batch_size, 1)
-    assert c.cy.shape == (*batch_size, 1)
-    assert c.height.shape == (*batch_size, 1)
-    assert c.width.shape == (*batch_size, 1)
-    assert c.distortion_params is None or c.distortion_params.shape == (*batch_size, 6)
-    assert c.camera_type.shape == (*batch_size, 1)
+def _check_cam_shapes(cam: Cameras, _batch_size):
+    if _batch_size:
+        assert len(cam) == _batch_size[0]
+    assert cam.shape == (*_batch_size,)
+    assert cam.camera_to_worlds.shape == (*_batch_size, 3, 4)
+    assert cam.fx.shape == (*_batch_size, 1)
+    assert cam.fy.shape == (*_batch_size, 1)
+    assert cam.cx.shape == (*_batch_size, 1)
+    assert cam.cy.shape == (*_batch_size, 1)
+    assert cam.height.shape == (*_batch_size, 1)
+    assert cam.width.shape == (*_batch_size, 1)
+    assert cam.distortion_params is None or cam.distortion_params.shape == (*_batch_size, 6)
+    assert cam.camera_type.shape == (*_batch_size, 1)
     return True
 
 
