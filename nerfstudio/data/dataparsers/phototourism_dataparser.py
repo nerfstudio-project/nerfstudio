500fafed5f5bc3999791f3c995489b80963460de
Jake Austin <jake-austin@berkeley.edu>
2022-11-08 08:11:52 +0000

    First pass dataparser

--- nerfstudio/data/dataparsers/phototourism_dataparser.py
+++ nerfstudio/data/dataparsers/phototourism_dataparser.py
@@ -19,8 +19,6 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Type
 
-import imageio
-import numpy as np
 import torch
 
 from nerfstudio.cameras.cameras import Cameras, CameraType
@@ -29,10 +27,7 @@ from nerfstudio.data.dataparsers.base_dataparser import (
     DataParserConfig,
     DataparserOutputs,
 )
-from nerfstudio.data.scene_box import SceneBox
-from nerfstudio.data.utils.colmap_utils import read_cameras_binary
-from nerfstudio.utils.colors import get_color
-from nerfstudio.utils.io import load_from_json
+from nerfstudio.data.utils.colmap_utils import read_cameras_binary, read_images_binary
 
 
 @dataclass
@@ -47,6 +42,8 @@ class PhototourismDataParserConfig(DataParserConfig):
     """How much to scale the camera origins by."""
     alpha_color: str = "white"
     """alpha color of background"""
+    split_frac: float = 0.8
+    """fraction of data to use for training"""
 
 
 @dataclass
@@ -62,49 +59,61 @@ class Phototourism(DataParser):
         self.data: Path = config.data
         self.scale_factor: float = config.scale_factor
         self.alpha_color = config.alpha_color
+        self.split_frac = config.split_frac
 
     def _generate_dataparser_outputs(self, split="train"):
-        # if self.alpha_color is not None:
-        #     alpha_color_tensor = get_color(self.alpha_color)
-        # else:
-        #     alpha_color_tensor = None
 
-        # meta = load_from_json(self.data / f"transforms_{split}.json")
-        # image_filenames = []
-        # poses = []
-        # for frame in meta["frames"]:
-        #     fname = self.data / Path(frame["file_path"].replace("./", "") + ".png")
-        #     image_filenames.append(fname)
-        #     poses.append(np.array(frame["transform_matrix"]))
-        # poses = np.array(poses).astype(np.float32)
+        cams = read_cameras_binary(self.data / "dense/sparse/cameras.bin")
+        imgs = read_images_binary(self.data / "dense/sparse/images.bin")
 
-        # img_0 = imageio.imread(image_filenames[0])
-        # image_height, image_width = img_0.shape[:2]
-        # camera_angle_x = float(meta["camera_angle_x"])
-        # focal_length = 0.5 * image_width / np.tan(0.5 * camera_angle_x)
+        cs = []
+        fxs = []
+        fys = []
+        cxs = []
+        cys = []
+        image_filenames = []
 
-        # cx = image_width / 2.0
-        # cy = image_height / 2.0
-        # camera_to_world = torch.from_numpy(poses[:, :3])  # camera to world transform
+        for _id in cams:
+            img = imgs[_id]
+            cam = cams[_id]
 
-        # # in x,y,z order
-        # camera_to_world[..., 3] *= self.scale_factor
-        # scene_box = SceneBox(aabb=torch.tensor([[-1.5, -1.5, -1.5], [1.5, 1.5, 1.5]], dtype=torch.float32))
+            assert cam.model == "PINHOLE", "Only pinhole (perspective) camera model is supported at the moment"
 
-        # cameras = Cameras(
-        #     camera_to_worlds=camera_to_world,
-        #     fx=focal_length,
-        #     fy=focal_length,
-        #     cx=cx,
-        #     cy=cy,
-        #     camera_type=CameraType.PERSPECTIVE,
-        # )
+            cs.append(torch.cat([torch.tensor(img.qvec2rotmat()), torch.tensor(img.tvec.reshape(3, 1))], dim=1))
+            fxs.append(cam.params[0])
+            fys.append(cam.params[1])
+            cxs.append(cam.params[2])
+            cys.append(cam.params[3])
 
-        # dataparser_outputs = DataparserOutputs(
-        #     image_filenames=image_filenames,
-        #     cameras=cameras,
-        #     alpha_color=alpha_color_tensor,
-        #     scene_box=scene_box,
-        # )
+            image_filenames.append(self.data / "dense/images" / img.name)
+
+        cs = torch.stack(cs)
+        fxs = torch.stack(fxs)
+        fys = torch.stack(fys)
+        cxs = torch.stack(cxs)
+        cys = torch.stack(cys)
+
+        cameras = Cameras(
+            camera_to_worlds=cs,
+            fx=fxs,
+            fy=fys,
+            cx=cxs,
+            cy=cys,
+            camera_type=CameraType.PERSPECTIVE,
+        )
+
+        train_idx = int(len(image_filenames) * self.split_frac)
+
+        if split == "train":
+            cameras = cameras[:train_idx]
+            image_filenames = image_filenames[:train_idx]
+        else:
+            cameras = cameras[train_idx:]
+            image_filenames = image_filenames[train_idx:]
+
+        dataparser_outputs = DataparserOutputs(
+            image_filenames=image_filenames,
+            cameras=cameras,
+        )
 
         return dataparser_outputs
