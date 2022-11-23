feb757206428f25eea7f8674a5c1474f784bb3f8
kadmin <julianknodt@gmail.com>
2022-11-23 03:01:54 +0000

    Add slider for render time

--- nerfstudio/viewer/app/src/modules/ConfigPanel/ConfigPanel.jsx
+++ nerfstudio/viewer/app/src/modules/ConfigPanel/ConfigPanel.jsx
@@ -1,5 +1,5 @@
 import { buttonGroup, useControls } from 'leva';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { WebSocketContext } from '../WebSocket/WebSocket';
@@ -26,6 +26,9 @@ export function RenderControls() {
   );
   const target_train_util = useSelector(
     (state) => state.renderingState.targetTrainUtil,
+  );
+  const render_time = useSelector(
+    (state) => state.renderingState.renderTime,
   );
 
   const dispatch = useDispatch();
@@ -106,6 +109,36 @@ export function RenderControls() {
     }
   };
 
+  const [display_render_time, set_display_render_time] = useState(false);
+
+  const receive_temporal_dist = e => {
+      const msg = msgpack.decode(new Uint8Array(e.data));
+      if (msg.path === "/model/has_temporal_distortion") {
+        set_display_render_time(msg.data === "true");
+        websocket.removeEventListener("message", receive_temporal_dist);
+      }
+  }
+  websocket.addEventListener('message', receive_temporal_dist);
+
+  const set_render_time = (value) => {
+    if (websocket.readyState === WebSocket.OPEN) {
+      const path = 'renderingState/render_time';
+      dispatch({
+        type: 'write',
+        path,
+        data: value,
+      });
+      const cmd = 'write';
+      const data = {
+        type: cmd,
+        path,
+        data: value,
+      };
+      const message = msgpack.encode(data);
+      websocket.send(message);
+    }
+  };
+
   const [, setControls] = useControls(
     () => ({
       // training speed
@@ -164,6 +197,17 @@ export function RenderControls() {
         '1024px': () => setControls({ max_resolution: 1024 }),
         '2048px': () => setControls({ max_resolution: 2048 }),
       }),
+      // Dynamic NeRF rendering time
+      ...(display_render_time ? {render_time: {
+        label: 'Render Timestep',
+        value: render_time,
+        min: 0,
+        max: 1,
+        step: 0.01,
+        onChange: (v) => {
+          set_render_time(v);
+        },
+      }} : {}),
     }),
     [
       outputOptions,
@@ -172,6 +216,8 @@ export function RenderControls() {
       colormapChoice,
       max_resolution,
       target_train_util,
+      render_time,
+      display_render_time,
       websocket, // need to re-render when websocket changes to use the new websocket
     ],
   );
@@ -188,6 +234,8 @@ export function RenderControls() {
     colormapChoice,
     max_resolution,
     target_train_util,
+    render_time,
+    display_render_time,
   ]);
 
   return null;
