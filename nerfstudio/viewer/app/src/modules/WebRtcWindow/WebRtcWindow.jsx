f3ed43190a2045c840190aea0ca7c443f301267b
Matt Tancik <tancik@berkeley.edu>
2022-11-23 11:54:02 -0800

    debugging webrtc failed connections

--- nerfstudio/viewer/app/src/modules/WebRtcWindow/WebRtcWindow.jsx
+++ nerfstudio/viewer/app/src/modules/WebRtcWindow/WebRtcWindow.jsx
@@ -18,6 +18,7 @@ export default function WebRtcWindow() {
   const getRTCPeerConnection = () => {
     const pc = new RTCPeerConnection({
       iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' },
         {
           urls: 'stun:openrelay.metered.ca:80',
         },
