b882b72e74dc6fd7fa3d623ea0a860c578617c99
Ole August <oleastole@gmail.com>
2022-11-26 15:10:59 +0100

    chore: abstract the writer

--- nerf_pipeline.py
+++ nerf_pipeline.py
@@ -26,10 +26,9 @@ class Args:
     percent_frames: float = 1.0
 
 class my_timer(ContextDecorator):
-    def __init__(self, name):
+    def __init__(self, write, name):
         self.name = name
-        self.terminal = sys.stdout
-        self.log = open("experiment_log.txt", "a")
+        self.write = write
 
     def __enter__(self):
         self.start = timer()
@@ -37,23 +36,26 @@ class my_timer(ContextDecorator):
     def __exit__(self, *args):
         self.end = timer()
         self.write(f"[time] {self.name}: {self.end - self.start}\n")
-    
-    def write(self, text: str):
-        self.terminal.write(text)
-        self.log.write(text)
-        self.log.flush()
-        self.terminal.flush()
+
 
 class ExperimentPipeline:
     def __init__(self, args: Args):
         self.args = args
+        self.terminal = sys.stdout
+        self.log = open("experiment_log.txt", "a")
 
     def run(self):
         self.process_data()
         self.train()
         train_output_dir, experiment_name = self.find_evaluate_paths()
         self.eval(train_output_dir, experiment_name)
-    
+            
+    def write(self, text: str):
+        self.terminal.write(text)
+        self.log.write(text)
+        self.log.flush()
+        self.terminal.flush()
+
     def find_evaluate_paths(self):
         experiment_name = '-'.join(str(self.args.output_dir).split('/'))
         train_output_dir = f"outputs/{experiment_name}/{self.args.model}"
@@ -61,7 +63,7 @@ class ExperimentPipeline:
         train_output_dir = os.path.join(train_output_dir, latest_changed_dir, "config.yml")
         return train_output_dir, experiment_name
 
-    @my_timer("Process")
+    @my_timer(write, "Process")
     def process_data(self):
         CONSOLE.print("Processing data")
         cmd = f"ns-process-data {self.args.data_source} --data {self.args.input_data_dir} --output-dir {self.args.output_dir}"
@@ -71,20 +73,21 @@ class ExperimentPipeline:
             total = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
             print(f"Num frames: {total}")
             num_frames = int(total * self.args.percent_frames)
+            self.write(f"Num frames: {num_frames} of {total}\n")
             cmd = f"ns-process-data {self.args.data_source} --data {self.args.input_data_dir} --output-dir {self.args.output_dir} --num-frames-target {num_frames}"
 
         print(cmd)
         run_command(cmd, verbose=True)
 
     # ns-train instant-ngp --data data/videos/tier2 --trainer.load_dir $output_path --viewer.start-train False
-    @my_timer("Train")
+    @my_timer(write, "Train")
     def train(self):
         input_data_dir = self.args.output_dir
         CONSOLE.print(f"Trainig model\nModel: {self.args.model}\nInput dir: {input_data_dir}")
         cmd = f"ns-train {self.args.model} --data {input_data_dir} --vis wandb --viewer.quit-on-train-completion True"
         run_command(cmd, verbose=True)
         
-    @my_timer("Evaluate")
+    @my_timer(write, "Evaluate")
     def eval(self, config: Path, output_name: Path):
         CONSOLE.print("Evaluating model")
         cmd = f"ns-eval --load-config {config} --output-path evals/{output_name}.json"
