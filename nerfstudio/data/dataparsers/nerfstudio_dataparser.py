6896028fc503b00947d04b7ed142a8dbd685749d
Nicolas Zunhammer <smashdroid@gmail.com>
2022-12-06 02:04:40 +0100

    Fix mask filenames misaligned after splitting set into train and eval (#1093)
    * Added Dockerfile to build docker image including all dependencies and nerfstudio, added description on how to user docker.
    
    * Move docker description to Installation.md.
    
    * Fix mask_filename indices after splitting train set.
    
    Co-authored-by: Nicolas Zunhammer <n.zunhammer@tum.de>

--- nerfstudio/data/dataparsers/nerfstudio_dataparser.py
+++ nerfstudio/data/dataparsers/nerfstudio_dataparser.py
@@ -196,6 +196,7 @@ class Nerfstudio(DataParser):
 
         # Choose image_filenames and poses based on split, but after auto orient and scaling the poses.
         image_filenames = [image_filenames[i] for i in indices]
+        mask_filenames = [mask_filenames[i] for i in indices]
         poses = poses[indices]
 
         # in x,y,z order
