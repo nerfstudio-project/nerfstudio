5cacc911426a132823d457ac1ee4832f3f37f344
Ethan Weber <eweb0124@gmail.com>
2023-01-10 23:22:20 +0000

    benchmark scripts

--- scripts/benchmarking/benchmark_nerfstudio_paper.py
+++ scripts/benchmarking/benchmark_nerfstudio_paper.py
@@ -21,12 +21,12 @@ mipnerf360_table_rows = [
         "nerfacto",
         "--pipeline.eval_optimize_cameras True --pipeline.eval_optimize_appearance False --pipeline.datamanager.camera-optimizer.mode off --pipeline.model.use-appearance-embedding False nerfstudio-data --downscale-factor 4 --train-split-percentage 0.875",
     ),
-    # TODO: instant-ngp method
-    # (
-    #     "instant-ngp-w/o-pose-app",
-    #     "instant-ngp",
-    #     "--pipeline.eval_optimize_cameras True --pipeline.eval_optimize_appearance False --pipeline.datamanager.camera-optimizer.mode off --pipeline.model.use-appearance-embedding False nerfstudio-data --downscale-factor 4 --train-split-percentage 0.875",
-    # ),
+    # instant-ngp method
+    (
+        "instant-ngp-w/o-pose-app",
+        "instant-ngp",
+        "--pipeline.eval_optimize_cameras True --pipeline.eval_optimize_appearance False --pipeline.datamanager.camera-optimizer.mode off --pipeline.model.use-appearance-embedding False nerfstudio-data --downscale-factor 4 --train-split-percentage 0.875",
+    ),
 ]
 
 
@@ -133,7 +133,7 @@ def main(capture_names, table_rows, data_path: Path = Path("data/nerfstudio")):
 
             def task():
                 print("Starting command: ", command)
-                out = run_command(command, verbose=False)
+                # out = run_command(command, verbose=False)
                 # time.sleep(5)
                 print("Finished command: ", command)
 
@@ -152,5 +152,6 @@ def main(capture_names, table_rows, data_path: Path = Path("data/nerfstudio")):
 
 
 if __name__ == "__main__":
-    main(mipnerf360_capture_names, mipnerf360_table_rows, data_path=Path("data/nerfstudio-data-mipnerf360"))
+    pass
+    # main(mipnerf360_capture_names, mipnerf360_table_rows, data_path=Path("data/nerfstudio-data-mipnerf360"))
     # main(ablations_capture_names, ablations_table_rows)
