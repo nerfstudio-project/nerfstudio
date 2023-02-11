6b2a0055e0999433e90f2f7b6d0be0cf2c764e31
DFin <d.finsterwalder@gmail.com>
2023-02-11 02:56:28 +0100

    RealityCapture export ambigous documentation Fixes #1398

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -737,7 +737,7 @@ class ProcessRealityCapture:
     """Process RealityCapture data into a nerfstudio dataset.
 
     This script assumes that cameras have been aligned using RealityCapture. After alignment, it is necessary to
-    export the camera poses as a `.csv` file.
+    export the camera poses as an `Internal/External camera paramaters (.csv)` file.
 
     This script does the following:
 
