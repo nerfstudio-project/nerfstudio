69b98459a5231db96a61afa3aefda40142335c81
terrance <terrance.3512@gmail.com>
2023-02-13 21:05:55 +0000

    add option to use random bg color + bg mlp

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -356,6 +356,7 @@ method_configs["dreamfusion"] = DreamfusionTrainerConfig(
             distortion_loss_mult=10,
             sphere_collider=True,
             initialize_density=False,
+            random_background=True,
             interlevel_loss_mult=1.0,
             proposal_warmup=500,
             proposal_update_every=5,
