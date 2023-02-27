30b8222b4c043b973101cfe5933db31f8cb7ce77
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2023-02-27 13:37:44 +0000

    feat(fields/semantic_nerf_field): Improve Type Hints

--- nerfstudio/fields/semantic_nerf_field.py
+++ nerfstudio/fields/semantic_nerf_field.py
@@ -49,14 +49,14 @@ class SemanticNerfField(Field):
 
     def __init__(
         self,
-        num_semantic_classes: int,
-        position_encoding: Encoding = Identity(in_dim=3),
-        direction_encoding: Encoding = Identity(in_dim=3),
-        base_mlp_num_layers: int = 8,
-        base_mlp_layer_width: int = 256,
-        head_mlp_num_layers: int = 2,
-        head_mlp_layer_width: int = 128,
-        skip_connections: Tuple[int] = (4,),
+        num_semantic_classes: Optional[int],
+        position_encoding: Optional[Encoding] = Identity(in_dim=3),
+        direction_encoding: Optional[Encoding] = Identity(in_dim=3),
+        base_mlp_num_layers: Optional[int] = 8,
+        base_mlp_layer_width: Optional[int] = 256,
+        head_mlp_num_layers: Optional[int] = 2,
+        head_mlp_layer_width: Optional[int] = 128,
+        skip_connections: Optional[Tuple[int]] = (4,),
     ) -> None:
         super().__init__()
         self.num_semantic_classes = num_semantic_classes
@@ -88,7 +88,7 @@ class SemanticNerfField(Field):
             in_dim=self.mlp_semantic.get_out_dim(), num_classes=self.num_semantic_classes
         )
 
-    def get_density(self, ray_samples: RaySamples):
+    def get_density(self, ray_samples: RaySamples) -> Tuple[TensorType, TensorType]:
         encoded_xyz = self.position_encoding(ray_samples.frustums.get_positions())
         base_mlp_out = self.mlp_base(encoded_xyz)
         density = self.field_head_density(base_mlp_out)
