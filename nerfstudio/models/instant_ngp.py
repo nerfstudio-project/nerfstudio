f48fa6a9238f367d53261746ce6cc2e3e9445266
Georgy Ponimatkin <38923619+ponimatkin@users.noreply.github.com>
2023-03-02 23:49:30 +0100

    Instant ngp args (#1549)
    * Expand instant-ngp args.
    
    * Fixed misplaced string.

--- nerfstudio/models/instant_ngp.py
+++ nerfstudio/models/instant_ngp.py
@@ -65,6 +65,10 @@ class InstantNGPModelConfig(ModelConfig):
     """Number of samples in field evaluation."""
     grid_resolution: int = 128
     """Resolution of the grid used for the field."""
+    max_res: int = 2048
+    """Maximum resolution of the hashmap for the base mlp."""
+    log2_hashmap_size: int = 19
+    """Size of the hashmap for the base mlp"""
     contraction_type: ContractionType = ContractionType.UN_BOUNDED_SPHERE
     """Contraction type used for spatial deformation of the field."""
     cone_angle: float = 0.004
@@ -103,6 +107,8 @@ class NGPModel(Model):
             contraction_type=self.config.contraction_type,
             use_appearance_embedding=self.config.use_appearance_embedding,
             num_images=self.num_train_data,
+            log2_hashmap_size=self.config.log2_hashmap_size,
+            max_res=self.config.max_res,
         )
 
         self.scene_aabb = Parameter(self.scene_box.aabb.flatten(), requires_grad=False)
