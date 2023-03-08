98bd2538bca96ca13b21f180e30a93e247dfd4c8
Frédéric Devernay <f-dy@users.noreply.github.com>
2023-03-07 19:02:00 -0800

    camera_utils: handle degenerate case for "vertical" orientation method

--- nerfstudio/cameras/camera_utils.py
+++ nerfstudio/cameras/camera_utils.py
@@ -436,13 +436,15 @@ def auto_orient_and_center_poses(
 ) -> Tuple[TensorType["num_poses":..., 3, 4], TensorType[4, 4]]:
     """Orients and centers the poses. We provide two methods for orientation: pca and up.
 
-    pca: Orient the poses so that the principal component of the points is aligned with the axes.
-        This method works well when all of the cameras are in the same plane.
+    pca: Orient the poses so that the principal directions of the camera centers are aligned
+        with the axes, Z corresponding to the smallest principal component.
+        This method works well when all of the cameras are in the same plane, for example when
+        images are taken using a mobile robot.
     up: Orient the poses so that the average up vector is aligned with the z axis.
         This method works well when images are not at arbitrary angles.
     vertical: Orient the poses so that the Z 3D direction projects close to the
         y axis in images. This method works better if cameras are not all
-        looking in the same 3D direction.
+        looking in the same 3D direction, which may happen in camera arrays or in LLFF.
 
     There are two centering methods:
     poses: The poses are centered around the origin.
@@ -515,10 +517,32 @@ def auto_orient_and_center_poses(
             # cameras by minimizing ||Xu|| s.t. ||u||=1. This total least squares
             # problem is solved by SVD.
             x_axis_matrix = poses[:, :3, 0]
-            _, _, Vh = torch.linalg.svd(x_axis_matrix, full_matrices=False)
-            up_vertical = Vh[2, :]
-            # It may be pointing up or down. Use "up" to disambiguate the sign.
-            up = up_vertical if torch.dot(up_vertical, up) > 0 else -up_vertical
+            _, S, Vh = torch.linalg.svd(x_axis_matrix, full_matrices=False)
+            # Singular values are S_i=||Xv_i|| for each right singular vector v_i.
+            # ||S|| = sqrt(n) because lines of X are all unit vectors and the v_i
+            # are an orthonormal basis.
+            # ||Xv_i|| = sqrt(sum(dot(x_axis_j,v_i)^2)), thus S_i/sqrt(n) is the
+            # RMS of cosines between x axes and v_i. If the second smallest singular
+            # value corresponds to an angle error less than 10° (cos(80°)=0.17),
+            # this is probably a degenerate camera configuration (typical values
+            # are around 5° average error for the true vertical). In this case,
+            # rather than taking the vector corresponding to the smallest singular
+            # value, we project the "up" vector on the plane spanned by the two
+            # best singular vectors. We could also just fallback to the "up"
+            # solution.
+            if S[1] > 0.17 * math.sqrt(poses.shape[0]):
+                # regular non-degenerate configuration
+                up_vertical = Vh[2, :]
+                # It may be pointing up or down. Use "up" to disambiguate the sign.
+                up = up_vertical if torch.dot(up_vertical, up) > 0 else -up_vertical
+            else:
+                # Degenerate configuration: project "up" on the plane spanned by
+                # the last two right singular vectors (which are orthogonal to the
+                # first). v_0 is a unit vector, no need to divide by its norm when
+                # projecting.
+                up = up - Vh[0, :] * torch.dot(up, Vh[0, :])
+                # re-normalize
+                up = up / torch.linalg.norm(up)
 
         rotation = rotation_matrix(up, torch.Tensor([0, 0, 1]))
         transform = torch.cat([rotation, rotation @ -translation[..., None]], dim=-1)
