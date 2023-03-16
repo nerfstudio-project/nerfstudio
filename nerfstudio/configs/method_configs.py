4c2e200098dda1c7afea38ec8e7eb92ae5938d21
David McAllister <davidrmc2@gmail.com>
2023-03-15 20:27:37 -0700

    TensoRF go brrrrrrr (#1609)
    * pdfsampler no longer includes uniform samples
    
    * Lint
    
    * Typo fix
    
    ---------
    
    Co-authored-by: Matthew Tancik <tancik@berkeley.edu>

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -310,6 +310,8 @@ method_configs["tensorf"] = TrainerConfig(
     pipeline=VanillaPipelineConfig(
         datamanager=VanillaDataManagerConfig(
             dataparser=BlenderDataParserConfig(),
+            train_num_rays_per_batch=4096,
+            eval_num_rays_per_batch=4096,
         ),
         model=TensoRFModelConfig(),
     ),
