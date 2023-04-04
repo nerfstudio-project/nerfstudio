ce2cf7bf7585d582ee7943d0f96d7a4660807514
Justin <justin.g.kerr@gmail.com>
2023-04-04 16:44:10 +0000

    remove lerf from main repo

--- projects/lerf/lerf/lerf_fieldheadnames.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-# from nerfstudio.field_components.field_heads import FieldHeadNames
-
-class LERFFieldHeadNames(Enum):
-    """Possible field outputs"""
-    HASHGRID = "hashgrid"
-    CLIP = "clip"
-    DINO = "dino"
