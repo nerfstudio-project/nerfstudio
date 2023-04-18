3d2e58ed8456e9cc72fd0844a9e05e30ab6295ab
Alexander Kristoffersen <akristoffersen@berkeley.edu>
2023-04-18 00:03:34 -0700

    Colab Fix: change localhost to 0.0.0.0 (#1759)
    Update viewer_state.py
    
    change localhost -> 0.0.0.0

--- nerfstudio/viewer/server/viewer_state.py
+++ nerfstudio/viewer/server/viewer_state.py
@@ -122,7 +122,7 @@ class ViewerState:
 
         self.camera_message = None
 
-        self.viser_server = ViserServer(host="localhost", port=websocket_port)
+        self.viser_server = ViserServer(host="0.0.0.0", port=websocket_port)
 
         self.viser_server.register_handler(IsTrainingMessage, self._handle_is_training)
         self.viser_server.register_handler(SaveCheckpointMessage, self._handle_save_checkpoint)
