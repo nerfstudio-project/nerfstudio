694679116203f9926e769c1b71a1b95dbcc3b2fc
Matt Tancik <mattt4001@gmail.com>
2023-04-18 16:40:54 -0700

    Add completed training state

--- nerfstudio/engine/trainer.py
+++ nerfstudio/engine/trainer.py
@@ -104,7 +104,7 @@ class Trainer:
         pipeline: The pipeline object.
         optimizers: The optimizers object.
         callbacks: The callbacks object.
-        is_training: Whether the model is training.
+        training_state: Current model training state.
     """
 
     pipeline: VanillaPipeline
@@ -118,7 +118,7 @@ class Trainer:
         self.world_size = world_size
         self.device: TORCH_DEVICE = "cpu" if world_size == 0 else f"cuda:{local_rank}"
         self.mixed_precision: bool = self.config.mixed_precision
-        self.is_training: bool = True
+        self.training_state: Literal["training", "paused", "completed"] = "training"
         if self.device == "cpu":
             self.mixed_precision = False
             CONSOLE.print("Mixed precision is disabled for CPU training.")
@@ -216,7 +216,7 @@ class Trainer:
             num_iterations = self.config.max_num_iterations
             step = 0
             for step in range(self._start_step, self._start_step + num_iterations):
-                while not self.is_training:
+                while self.training_state == "paused":
                     time.sleep(0.01)
                 with self.train_lock:
                     with TimeWriter(writer, EventName.ITER_TRAIN_TIME, step=step) as train_t:
@@ -280,6 +280,8 @@ class Trainer:
         CONSOLE.rule()
         CONSOLE.print("[bold green]:tada: :tada: :tada: Training Finished :tada: :tada: :tada:", justify="center")
         if not self.config.viewer.quit_on_train_completion:
+            self.training_state = "completed"
+            self._train_complete_viewer()
             CONSOLE.print("Use ctrl+c to quit", justify="center")
             while True:
                 time.sleep(0.01)
@@ -304,7 +306,7 @@ class Trainer:
         assert self.viewer_state and self.pipeline.datamanager.train_dataset
         self.viewer_state.init_scene(
             dataset=self.pipeline.datamanager.train_dataset,
-            start_train=True,
+            train_state="training",
         )
 
     @check_viewer_enabled
@@ -319,6 +321,16 @@ class Trainer:
         num_rays_per_batch: int = self.pipeline.datamanager.get_train_rays_per_batch()
         try:
             self.viewer_state.update_scene(step, num_rays_per_batch)
+        except RuntimeError:
+            time.sleep(0.03)  # sleep to allow buffer to reset
+            CONSOLE.log("Viewer failed. Continuing training.")
+
+    @check_viewer_enabled
+    def _train_complete_viewer(self) -> None:
+        """Let the viewer know that the training is complete"""
+        assert self.viewer_state is not None
+        try:
+            self.viewer_state.training_complete()
         except RuntimeError:
             time.sleep(0.03)  # sleep to allow buffer to reset
             CONSOLE.log("Viewer failed. Continuing training.")
