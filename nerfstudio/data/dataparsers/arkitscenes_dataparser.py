1bdab5ecb6ec99d0cfb595fc78d03ad4a3d5dae0
terrance <terrance.3512@gmail.com>
2023-05-12 19:07:03 +0000

    undo chagnes in data folder

--- nerfstudio/data/dataparsers/arkitscenes_dataparser.py
+++ nerfstudio/data/dataparsers/arkitscenes_dataparser.py
@@ -21,7 +21,6 @@ from typing import Literal, Type
 import cv2
 import numpy as np
 import torch
-from typing_extensions import Literal
 
 from nerfstudio.cameras import camera_utils
 from nerfstudio.cameras.cameras import Cameras, CameraType
