2ed6ccb140111f8c14bc51623893edf4e738cafb
Jonas Kulhanek <jonas.kulhanek@live.com>
2023-05-15 19:11:28 +0200

    Fix missing _LRScheduler import

--- nerfstudio/engine/schedulers.py
+++ nerfstudio/engine/schedulers.py
@@ -25,6 +25,12 @@ from torch.optim import Optimizer, lr_scheduler
 
 from nerfstudio.configs.base_config import InstantiateConfig
 
+try:
+    from torch.optim.lr_scheduler import LRScheduler
+except ImportError:
+    # Backward compatibility for PyTorch 1.x
+    from torch.optim.lr_scheduler import _LRScheduler as LRScheduler
+
 
 @dataclass
 class SchedulerConfig(InstantiateConfig):
@@ -44,7 +50,7 @@ class Scheduler:
         self.config = config
 
     @abstractmethod
-    def get_scheduler(self, optimizer: Optimizer, lr_init: float) -> lr_scheduler._LRScheduler:
+    def get_scheduler(self, optimizer: Optimizer, lr_init: float) -> LRScheduler:
         """Abstract method that returns a scheduler object.
 
         Args:
@@ -74,7 +80,7 @@ class MultiStepScheduler(Scheduler):
 
     config: MultiStepSchedulerConfig
 
-    def get_scheduler(self, optimizer: Optimizer, lr_init: float) -> lr_scheduler._LRScheduler:
+    def get_scheduler(self, optimizer: Optimizer, lr_init: float) -> LRScheduler:
         scheduler = lr_scheduler.MultiStepLR(
             optimizer=optimizer,
             milestones=self.config.milestones,
@@ -108,7 +114,7 @@ class ExponentialDecayScheduler(Scheduler):
 
     config: ExponentialDecaySchedulerConfig
 
-    def get_scheduler(self, optimizer: Optimizer, lr_init: float) -> lr_scheduler._LRScheduler:
+    def get_scheduler(self, optimizer: Optimizer, lr_init: float) -> LRScheduler:
         if self.config.lr_final is None:
             lr_final = lr_init
         else:
@@ -155,7 +161,7 @@ class CosineDecayScheduler(Scheduler):
 
     config: CosineDecaySchedulerConfig
 
-    def get_scheduler(self, optimizer: Optimizer, lr_init: float) -> lr_scheduler._LRScheduler:
+    def get_scheduler(self, optimizer: Optimizer, lr_init: float) -> LRScheduler:
         def func(step):
             if step < self.config.warm_up_end:
                 learning_factor = step / self.config.warm_up_end
