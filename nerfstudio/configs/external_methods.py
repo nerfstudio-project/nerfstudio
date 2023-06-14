d64e17c32b84cbd58a5b2deae0b6138c5b2a3e50
lsongx <12553253+lsongx@users.noreply.github.com>
2023-06-13 23:10:26 -0700

    update

--- nerfstudio/configs/external_methods.py
+++ nerfstudio/configs/external_methods.py
@@ -109,11 +109,11 @@ To enable Tetra-NeRF, you must install it first. Please follow the instructions 
 # NeRFPlayer
 external_methods.append(
     ExternalMethod(
-        """[bold yellow]Tetra-NeRF[/bold yellow]
+        """[bold yellow]NeRFPlayer[/bold yellow]
 For more information visit: https://docs.nerf.studio/en/latest/nerfology/methods/nerfplayer.html
 
-To enable NeRFPlayer, you must install it first. Please follow the instructions here:
-  https://github.com/lsongx/nerfplayer-nerfstudio#installation""",
+To enable NeRFPlayer, you must install it first by running:
+  [grey]pip install git+https://github.com/lsongx/nerfplayer-nerfstudio[/grey]""",
         configurations=[
             ("nerfplayer-nerfacto", "NeRFPlayer with nerfacto backbone"),
             ("nerfplayer-ngp", "NeRFPlayer with instang-ngp-bounded backbone"),
