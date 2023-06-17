1066f8f4330f69363e24984ddf8dc2e7299a23ff
machenmusik <machenmusik@users.noreply.github.com>
2023-06-17 02:49:56 -0400

    don't force downscale factor to 1 for nerfacto-huge  (#2088)
    * per @kerrj, nerfacto-huge doesn't need downscale_factor=1

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -158,7 +158,7 @@ method_configs["nerfacto-huge"] = TrainerConfig(
     mixed_precision=True,
     pipeline=VanillaPipelineConfig(
         datamanager=VanillaDataManagerConfig(
-            dataparser=NerfstudioDataParserConfig(downscale_factor=1),
+            dataparser=NerfstudioDataParserConfig(),
             train_num_rays_per_batch=16384,
             eval_num_rays_per_batch=4096,
             camera_optimizer=CameraOptimizerConfig(
