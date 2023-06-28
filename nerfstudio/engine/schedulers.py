493ebefb202efc6945e0ebb7997bd06aacf919f2
TriptSharma <tript35.sharma@gmail.com>
2023-06-28 18:35:18 +0000

    add multi step warmup scheduler

--- nerfstudio/engine/schedulers.py
+++ nerfstudio/engine/schedulers.py
@@ -17,7 +17,7 @@
 
 from abc import abstractmethod
 from dataclasses import dataclass, field
-from typing import Literal, Optional, Tuple, Type
+from typing import Literal, Optional, Tuple, Type, List, Any
 
 import numpy as np
 from torch.optim import Optimizer, lr_scheduler
@@ -168,6 +168,42 @@ class CosineDecayScheduler(Scheduler):
                 alpha = self.config.learning_rate_alpha
                 progress = (step - self.config.warm_up_end) / (self.config.max_steps - self.config.warm_up_end)
                 learning_factor = (np.cos(np.pi * progress) + 1.0) * 0.5 * (1 - alpha) + alpha
+            return learning_factor
+
+        scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=func)
+        return scheduler
+
+
+@dataclass
+class MultiStepWarmupSchedulerConfig(InstantiateConfig):
+    """Basic scheduler config with self-defined exponential decay schedule"""
+
+    _target: Type = field(default_factory=lambda: MultiStepWarmupScheduler)
+    warm_up_end: int = 5000
+    milestones: List[int] = field(default_factory=lambda: [300000, 400000, 500000])
+    gamma: float = 0.33
+    
+    # def __init__(self, optimizer=None, **kwargs) -> Any:
+    #     """Returns the instantiated object using the config."""
+    #     return self._target(
+    #         optimizer,
+    #         self.warm_up_end,
+    #         self.milestones,
+    #         self.gamma
+    #     )
+
+class MultiStepWarmupScheduler(Scheduler):
+    """Starts with a flat lr schedule until it reaches N epochs then applies a given scheduler"""
+
+    config: MultiStepWarmupSchedulerConfig
+
+    def get_scheduler(self, optimizer: Optimizer, lr_init: float) -> LRScheduler:
+        def func(step):
+            if step < self.config.warm_up_end:
+                learning_factor = step / self.config.warm_up_end
+            else:
+                index = np.searchsorted(self.config.milestones, step, side='left')
+                learning_factor = self.config.gamma ** index
             return learning_factor
 
         scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=func)
