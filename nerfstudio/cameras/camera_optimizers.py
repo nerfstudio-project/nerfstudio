4ddbee9b0824a93497bd5063bcf8b5c6640cca85
maturk <matiasturkulainen@gmail.com>
2023-07-20 18:23:46 +0300

    type ignore

--- nerfstudio/cameras/camera_optimizers.py
+++ nerfstudio/cameras/camera_optimizers.py
@@ -113,7 +113,7 @@ class CameraOptimizer(nn.Module):
     def apply_to_raybundle(self, raybundle: RayBundle) -> None:
         """Apply the pose correction to the raybundle"""
         if self.config.mode != "off":
-            correction_matrices = self(raybundle.camera_indices.squeeze())
+            correction_matrices = self(raybundle.camera_indices.squeeze())  # type: ignore
             raybundle.origins = raybundle.origins + correction_matrices[:, :3, 3]
             raybundle.directions = torch.bmm(correction_matrices[:, :3, :3], raybundle.directions[..., None]).squeeze()
 
