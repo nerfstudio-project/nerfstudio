86cb5cee7926b4bd5fdb31e7771a6f37b26be63c
AX-I <36831950+AX-I@users.noreply.github.com>
2023-08-01 11:44:32 -0400

    Convert to config option

--- nerfstudio/data/datamanagers/base_datamanager.py
+++ nerfstudio/data/datamanagers/base_datamanager.py
@@ -117,6 +117,8 @@ class DataManagerConfig(InstantiateConfig):
     """Specifies the camera pose optimizer used during training. Helpful if poses are noisy."""
     masks_on_gpu: Optional[bool] = None
     """Process masks on GPU for speed at the expense of memory, if True."""
+    images_on_gpu: Optional[bool] = None
+    """Process images on GPU for speed at the expense of memory, if True."""
 
 
 class DataManager(nn.Module):
@@ -403,6 +405,8 @@ class VanillaDataManager(DataManager, Generic[TDataset]):
         self.exclude_batch_keys_from_device = self.train_dataset.exclude_batch_keys_from_device
         if self.config.masks_on_gpu is True:
             self.exclude_batch_keys_from_device.remove("mask")
+        if self.config.images_on_gpu is True:
+            self.exclude_batch_keys_from_device.remove("image")
 
         if self.train_dataparser_outputs is not None:
             cameras = self.train_dataparser_outputs.cameras
