c11a43f249b7d722eed8e735245a0580624d51d1
Ethan Weber <ethanweber@berkeley.edu>
2023-08-04 20:16:17 +0000

    support for patch pixel samplerr masks

--- nerfstudio/data/pixel_samplers.py
+++ nerfstudio/data/pixel_samplers.py
@@ -285,7 +285,7 @@ class PatchPixelSamplerConfig(PixelSamplerConfig):
 
     _target: Type = field(default_factory=lambda: PatchPixelSampler)
     """Target class to instantiate."""
-    patch_size: int = 2
+    patch_size: int = 32
     """Side length of patch. This must be consistent in the method
     config in order for samples to be reshaped into patches correctly."""
 
@@ -319,9 +319,27 @@ class PatchPixelSampler(PixelSampler):
         device: Union[torch.device, str] = "cpu",
     ) -> Int[Tensor, "batch_size 3"]:
         if isinstance(mask, Tensor):
-            raise NotImplementedError(
-                "Masked sampling not implemented for PairPixelSampler. Change Config to PixelSamplerConfig instead."
+            sub_bs = batch_size // (self.config.patch_size**2)
+            half_patch_size = int(self.config.patch_size / 2)
+            m = erode_mask(mask.permute(0, 3, 1, 2).float(), pixel_radius=half_patch_size)
+            nonzero_indices = torch.nonzero(m[:, 0], as_tuple=False).to(device)
+            chosen_indices = random.sample(range(len(nonzero_indices)), k=self.rays_to_sample)
+            indices = nonzero_indices[chosen_indices]
+
+            indices = (
+                indices.view(sub_bs, 1, 1, 3)
+                .broadcast_to(sub_bs, self.config.patch_size, self.config.patch_size, 3)
+                .clone()
             )
+
+            yys, xxs = torch.meshgrid(
+                torch.arange(self.config.patch_size, device=device), torch.arange(self.config.patch_size, device=device)
+            )
+            indices[:, ..., 1] += yys - half_patch_size
+            indices[:, ..., 2] += xxs - half_patch_size
+
+            indices = torch.floor(indices).long()
+            indices = indices.flatten(0, 2)
         else:
             sub_bs = batch_size // (self.config.patch_size**2)
             indices = torch.rand((sub_bs, 3), device=device) * torch.tensor(
@@ -382,8 +400,7 @@ class PairPixelSampler(PixelSampler):  # pylint: disable=too-few-public-methods
         device: Union[torch.device, str] = "cpu",
     ) -> Int[Tensor, "batch_size 3"]:
         if isinstance(mask, Tensor):
-            m = mask.permute(0, 3, 1, 2).float()
-            m = erode_mask(m, pixel_radius=self.radius)
+            m = erode_mask(mask.permute(0, 3, 1, 2).float(), pixel_radius=self.radius)
             nonzero_indices = torch.nonzero(m[:, 0], as_tuple=False).to(device)
             chosen_indices = random.sample(range(len(nonzero_indices)), k=self.rays_to_sample)
             indices = nonzero_indices[chosen_indices]
