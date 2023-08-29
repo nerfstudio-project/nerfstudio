959a378479c30c8430f9e09522ab53b001433c2a
maturk <matiasturkulainen@gmail.com>
2023-08-29 17:14:58 +0300

    gaussian pipeline

---
+++ nerfstudio/pipelines/gaussian_pipeline.py
@@ -0,0 +1,155 @@
+# Copyright 2022 the Regents of the University of California, Nerfstudio Team and contributors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Abstracts for the Pipeline class.
+"""
+from __future__ import annotations
+
+import typing
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Dict, List, Literal, Optional, Type
+
+import torch.distributed as dist
+from torch.nn import Parameter
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.cuda.amp.grad_scaler import GradScaler
+
+from nerfstudio.configs import base_config as cfg
+from nerfstudio.data.datamanagers.base_datamanager import (
+    DataManager,
+    DataManagerConfig,
+)
+from nerfstudio.engine.callbacks import TrainingCallback, TrainingCallbackAttributes
+from nerfstudio.models.base_model import Model, ModelConfig
+from nerfstudio.utils import profiler
+from nerfstudio.pipelines.base_pipeline import Pipeline
+
+from nerfstudio.data.datamanagers.gaussian_splatting_datamanager import RasterizerDataManagerConfig
+from nerfstudio.models.gaussian_splatting import GaussianSplattingModelConfig
+
+
+@dataclass
+class GaussianPipelineConfig(cfg.InstantiateConfig):
+    """Configuration for pipeline instantiation"""
+
+    _target: Type = field(default_factory=lambda: GaussianPipeline)
+    """target class to instantiate"""
+    datamanager: DataManagerConfig = RasterizerDataManagerConfig()
+    """specifies the datamanager config"""
+    model: ModelConfig = GaussianSplattingModelConfig()
+    """specifies the model config"""
+
+
+class GaussianPipeline(Pipeline):
+    def __init__(
+        self,
+        config: GaussianPipelineConfig,
+        device: str,
+        test_mode: Literal["test", "val", "inference"] = "val",
+        world_size: int = 1,
+        local_rank: int = 0,
+        grad_scaler: Optional[GradScaler] = None,
+    ):
+        super().__init__()
+        self.config = config
+        self.test_mode = test_mode
+        self.datamanager: DataManager = config.datamanager.setup(
+            device=device, test_mode=test_mode, world_size=world_size, local_rank=local_rank
+        )
+        self.datamanager.to(device)
+        # TODO(ethan): get rid of scene_bounds from the model
+        assert self.datamanager.train_dataset is not None, "Missing input dataset"
+
+        self.cameras = self.datamanager.train_dataset.cameras
+        self.gaussians = self.datamanager.gaussians
+
+        self._model = config.model.setup(
+            scene_box=self.datamanager.train_dataset.scene_box,
+            num_train_data=len(self.datamanager.train_dataset),
+            metadata=self.datamanager.train_dataset.metadata,
+            device=device,
+            grad_scaler=grad_scaler,
+            cameras=self.cameras,
+            gaussians=self.gaussians,
+        )
+        self.model.to(device)
+
+        self.world_size = world_size
+        if world_size > 1:
+            self._model = typing.cast(Model, DDP(self._model, device_ids=[local_rank], find_unused_parameters=True))
+            dist.barrier(device_ids=[local_rank])
+
+    @profiler.time_function
+    def get_train_loss_dict(self, step: int):
+        """This function gets your training loss dict. This will be responsible for
+        getting the next batch of data from the DataManager and interfacing with the
+        Model class, feeding the data to the model's forward function.
+
+        Args:
+            step: current iteration step to update sampler if using DDP (distributed)
+        """
+        if self.world_size > 1 and step:
+            assert self.datamanager.train_sampler is not None
+            self.datamanager.train_sampler.set_epoch(step)
+        idx, image = self.datamanager.next_train(step)
+        model_outputs = self.model(idx, image)
+        metrics_dict = self.model.get_metrics_dict(model_outputs, batch)
+        loss_dict = self.model.get_loss_dict(model_outputs, batch, metrics_dict)
+
+        return model_outputs, loss_dict, metrics_dict
+
+    @profiler.time_function
+    def get_eval_image_metrics_and_images(self, step: int):
+        """This function gets your evaluation loss dict. It needs to get the data
+        from the DataManager and feed it to the model's forward function
+
+        Args:
+            step: current iteration step
+        """
+        return None
+
+    @profiler.time_function
+    def get_average_eval_image_metrics(
+        self, step: Optional[int] = None, output_path: Optional[Path] = None, get_std: bool = False
+    ):
+        """Iterate over all the images in the eval dataset and get the average.
+
+        Args:
+            step: current training step
+            output_path: optional path to save rendered images to
+            get_std: Set True if you want to return std with the mean metric.
+        """
+        return None
+
+    def get_training_callbacks(
+        self, training_callback_attributes: TrainingCallbackAttributes
+    ) -> List[TrainingCallback]:
+        """Returns the training callbacks from both the Dataloader and the Model."""
+        datamanager_callbacks = self.datamanager.get_training_callbacks(training_callback_attributes)
+        model_callbacks = self.model.get_training_callbacks(training_callback_attributes)
+        callbacks = datamanager_callbacks + model_callbacks
+        return callbacks
+
+    def get_param_groups(self) -> Dict[str, List[Parameter]]:
+        """Get the param groups for the pipeline.
+
+        Returns:
+            A list of dictionaries containing the pipeline's param groups.
+        """
+        datamanager_params = self.datamanager.get_param_groups()
+        model_params = self.model.get_param_groups()
+        # TODO(ethan): assert that key names don't overlap
+        return {**datamanager_params, **model_params}
