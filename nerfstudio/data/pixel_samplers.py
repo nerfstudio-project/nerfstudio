805e6e702d328bf6fc2bf9721cbb0a4eedf03a35
anc2001 <71151378+anc2001@users.noreply.github.com>
2023-09-14 09:11:15 -0700

    fix pixel sampling with masks issue when data is list (#2369)
    Co-authored-by: Brent Yi <yibrenth@gmail.com>

--- nerfstudio/data/pixel_samplers.py
+++ nerfstudio/data/pixel_samplers.py
@@ -215,7 +215,7 @@ class PixelSampler:
                     num_rays_in_batch = num_rays_per_batch - (num_images - 1) * num_rays_in_batch
 
                 indices = self.sample_method(
-                    num_rays_in_batch, 1, image_height, image_width, mask=batch["mask"][i], device=device
+                    num_rays_in_batch, 1, image_height, image_width, mask=batch["mask"][i].unsqueeze(0), device=device
                 )
                 indices[:, 0] = i
                 all_indices.append(indices)
