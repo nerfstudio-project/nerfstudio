44528550d6f4bd43b6ef5768c978db106692a777
Matthew Tancik <mattt4001@gmail.com>
2023-10-08 18:32:10 -0700

    Fix black

--- nerfstudio/field_components/encodings.py
+++ nerfstudio/field_components/encodings.py
@@ -482,9 +482,7 @@ class TensorCPEncoding(Encoding):
             resolution: Target resolution.
         """
 
-        line_coef = F.interpolate(
-            self.line_coef.data, size=(resolution, 1), mode="bilinear", align_corners=True
-        )
+        line_coef = F.interpolate(self.line_coef.data, size=(resolution, 1), mode="bilinear", align_corners=True)
         self.line_coef = torch.nn.Parameter(line_coef)
 
         self.resolution = resolution
