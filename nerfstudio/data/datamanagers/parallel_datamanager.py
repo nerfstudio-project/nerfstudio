39b2dc5901dc0f1e8c48ba5b3db28f3ace247ebf
Haithem Turki <hturki@cs.cmu.edu>
2024-02-07 19:03:39 -0500

    run ruff

--- nerfstudio/data/datamanagers/parallel_datamanager.py
+++ nerfstudio/data/datamanagers/parallel_datamanager.py
@@ -111,9 +111,7 @@ class DataProcessor(mp.Process):  # type: ignore
         self.config = config
         self.dataparser_outputs = dataparser_outputs
         self.dataset = dataset
-        self.exclude_batch_keys_from_device = (
-            self.dataset.exclude_batch_keys_from_device
-        )
+        self.exclude_batch_keys_from_device = self.dataset.exclude_batch_keys_from_device
         self.pixel_sampler = pixel_sampler
         self.ray_generator = RayGenerator(self.dataset.cameras)
         self.cache_images()
@@ -135,24 +133,18 @@ class DataProcessor(mp.Process):  # type: ignore
                     time.sleep(0.0001)
                 except Exception:
                     CONSOLE.print_exception()
-                    CONSOLE.print(
-                        "[bold red]Error: Error occurred in parallel datamanager queue."
-                    )
+                    CONSOLE.print("[bold red]Error: Error occurred in parallel datamanager queue.")
 
     def cache_images(self):
         """Caches all input images into a NxHxWx3 tensor."""
         indices = range(len(self.dataset))
         batch_list = []
         results = []
-        with concurrent.futures.ThreadPoolExecutor(
-            max_workers=self.config.max_thread_workers
-        ) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.config.max_thread_workers) as executor:
             for idx in indices:
                 res = executor.submit(self.dataset.__getitem__, idx)
                 results.append(res)
-            for res in track(
-                results, description="Loading data batch", transient=False
-            ):
+            for res in track(results, description="Loading data batch", transient=False):
                 batch_list.append(res.result())
         self.img_data = self.config.collate_fn(batch_list)
 
@@ -188,27 +180,18 @@ class ParallelDataManager(DataManager, Generic[TDataset]):
         if test_mode == "inference":
             self.dataparser.downscale_factor = 1  # Avoid opening images
         self.includes_time = self.dataparser.includes_time
-        self.train_dataparser_outputs: DataparserOutputs = (
-            self.dataparser.get_dataparser_outputs(split="train")
-        )
-        self.eval_dataparser_outputs: DataparserOutputs = (
-            self.dataparser.get_dataparser_outputs(split=self.test_split)
-        )
+        self.train_dataparser_outputs: DataparserOutputs = self.dataparser.get_dataparser_outputs(split="train")
+        self.eval_dataparser_outputs: DataparserOutputs = self.dataparser.get_dataparser_outputs(split=self.test_split)
         cameras = self.train_dataparser_outputs.cameras
         if len(cameras) > 1:
             for i in range(1, len(cameras)):
-                if (
-                    cameras[0].width != cameras[i].width
-                    or cameras[0].height != cameras[i].height
-                ):
+                if cameras[0].width != cameras[i].width or cameras[0].height != cameras[i].height:
                     CONSOLE.print("Variable resolution, using variable_res_collate")
                     self.config.collate_fn = variable_res_collate
                     break
         self.train_dataset = self.create_train_dataset()
         self.eval_dataset = self.create_eval_dataset()
-        self.exclude_batch_keys_from_device = (
-            self.train_dataset.exclude_batch_keys_from_device
-        )
+        self.exclude_batch_keys_from_device = self.train_dataset.exclude_batch_keys_from_device
         # Spawn is critical for not freezing the program (PyTorch compatability issue)
         # check if spawn is already set
         if mp.get_start_method(allow_none=True) is None:  # type: ignore
@@ -250,30 +233,19 @@ class ParallelDataManager(DataManager, Generic[TDataset]):
     def create_eval_dataset(self) -> TDataset:
         """Sets up the data loaders for evaluation."""
         return self.dataset_type(
-            dataparser_outputs=self.dataparser.get_dataparser_outputs(
-                split=self.test_split
-            ),
+            dataparser_outputs=self.dataparser.get_dataparser_outputs(split=self.test_split),
             scale_factor=self.config.camera_res_scale_factor,
         )
 
-    def _get_pixel_sampler(
-        self, dataset: TDataset, num_rays_per_batch: int
-    ) -> PixelSampler:
+    def _get_pixel_sampler(self, dataset: TDataset, num_rays_per_batch: int) -> PixelSampler:
         """Infer pixel sampler to use."""
-        if (
-            self.config.patch_size > 1
-            and type(self.config.pixel_sampler) is PixelSamplerConfig
-        ):
+        if self.config.patch_size > 1 and type(self.config.pixel_sampler) is PixelSamplerConfig:
             return PatchPixelSamplerConfig().setup(
                 patch_size=self.config.patch_size, num_rays_per_batch=num_rays_per_batch
             )
-        is_equirectangular = (
-            dataset.cameras.camera_type == CameraType.EQUIRECTANGULAR.value
-        ).all()
+        is_equirectangular = (dataset.cameras.camera_type == CameraType.EQUIRECTANGULAR.value).all()
         if is_equirectangular.any():
-            CONSOLE.print(
-                "[bold yellow]Warning: Some cameras are equirectangular, but using default pixel sampler."
-            )
+            CONSOLE.print("[bold yellow]Warning: Some cameras are equirectangular, but using default pixel sampler.")
 
         fisheye_crop_radius = None
         if dataset.cameras.metadata is not None:
@@ -320,9 +292,7 @@ class ParallelDataManager(DataManager, Generic[TDataset]):
         )
         self.iter_eval_image_dataloader = iter(self.eval_image_dataloader)
         self.eval_pixel_sampler = self._get_pixel_sampler(self.eval_dataset, self.config.eval_num_rays_per_batch)  # type: ignore
-        self.eval_ray_generator = RayGenerator(
-            self.eval_dataset.cameras.to(self.device)
-        )
+        self.eval_ray_generator = RayGenerator(self.eval_dataset.cameras.to(self.device))
         # for loading full images
         self.fixed_indices_eval_dataloader = FixedIndicesEvalDataloader(
             input_dataset=self.eval_dataset,
