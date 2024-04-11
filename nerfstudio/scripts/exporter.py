0001c048a8e0118458ec998ca302b7aaa774e0c5
Brent Yi <yibrenth@gmail.com>
2024-04-10 17:34:58 -0700

    sort imports

--- nerfstudio/scripts/exporter.py
+++ nerfstudio/scripts/exporter.py
@@ -35,8 +35,8 @@ from typing_extensions import Annotated, Literal
 
 from nerfstudio.cameras.rays import RayBundle
 from nerfstudio.data.datamanagers.base_datamanager import VanillaDataManager
-from nerfstudio.data.datamanagers.parallel_datamanager import ParallelDataManager
 from nerfstudio.data.datamanagers.full_images_datamanager import FullImageDatamanager
+from nerfstudio.data.datamanagers.parallel_datamanager import ParallelDataManager
 from nerfstudio.data.datamanagers.random_cameras_datamanager import RandomCamerasDataManager
 from nerfstudio.data.scene_box import OrientedBox
 from nerfstudio.exporter import texture_utils, tsdf_utils
