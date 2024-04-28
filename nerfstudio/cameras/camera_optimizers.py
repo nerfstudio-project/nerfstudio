196d8344c958027b5f5e4ade3db3afd41ca1abb4
Otto Seiskari <otto.seiskari@gmail.com>
2024-04-28 12:19:19 +0300

    Fix exported poses when Splatfacto pose optimization is enabled
    Currently works correctly only with --eval-mode=all

--- nerfstudio/cameras/camera_optimizers.py
+++ nerfstudio/cameras/camera_optimizers.py
@@ -157,14 +157,13 @@ class CameraOptimizer(nn.Module):
         if self.config.mode == "off":
             return camera.camera_to_worlds
 
-        assert camera.metadata is not None, "Must provide id of camera in its metadata"
-        if "cam_idx" not in camera.metadata:
-            # Evalutaion cams?
+        if camera.metadata is None or "cam_idx" not in camera.metadata:
+            # Viser cameras
             return camera.camera_to_worlds
 
         camera_idx = camera.metadata["cam_idx"]
         adj = self(torch.tensor([camera_idx], dtype=torch.long, device=camera.device))  # type: ignore
-        adj = torch.cat([adj, torch.Tensor([0, 0, 0, 1])[None, None].to(adj)], dim=1)
+        adj = torch.cat([adj, torch.Tensor([0, 0, 0, 1])[None, None].to(adj)], dim=1).to(camera.camera_to_worlds.device)
         return torch.bmm(camera.camera_to_worlds, adj)
 
     def get_loss_dict(self, loss_dict: dict) -> None:
