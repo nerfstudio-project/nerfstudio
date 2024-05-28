36650ef6e7907ef0718a00207bb3b53d8217a2f2
Justin Kerr <justin.g.kerr@gmail.com>
2024-05-28 14:21:39 -0700

    batch viewmats

--- nerfstudio/models/splatfacto.py
+++ nerfstudio/models/splatfacto.py
@@ -98,23 +98,22 @@ def resize_image(image: torch.Tensor, d: int):
     return tf.conv2d(image.permute(2, 0, 1)[:, None, ...], weight, stride=d).squeeze(1).permute(1, 2, 0)
 
 
-@torch.compile(dynamic=False)
+@torch.compile()
 def get_viewmat(optimized_camera_to_world):
     """
     function that converts c2w to gsplat world2camera matrix, using compile for some speed
     """
-    # shift the camera to center of scene looking at center
-    R = optimized_camera_to_world[:3, :3]  # 3 x 3
-    T = optimized_camera_to_world[:3, 3:4]  # 3 x 1
-
+    R = optimized_camera_to_world[:,:3, :3]  # 3 x 3
+    T = optimized_camera_to_world[:,:3, 3:4]  # 3 x 1
     # flip the z and y axes to align with gsplat conventions
-    R = R * torch.tensor([[1, -1, -1]], device=R.device, dtype=R.dtype)
+    R = R * torch.tensor([[[1, -1, -1]]], device=R.device, dtype=R.dtype)
     # analytic matrix inverse to get world2camera matrix
-    R_inv = R.T
-    T_inv = -R_inv @ T
-    viewmat = torch.eye(4, device=R.device, dtype=R.dtype)
-    viewmat[:3, :3] = R_inv
-    viewmat[:3, 3:4] = T_inv
+    R_inv = R.transpose(1,2)
+    T_inv = -torch.bmm(R_inv, T)
+    viewmat = torch.zeros(R.shape[0],4, 4, device=R.device, dtype=R.dtype)
+    viewmat[:,3,3] = 1.0#homogenous
+    viewmat[:,:3, :3] = R_inv
+    viewmat[:,:3, 3:4] = T_inv
     return viewmat
 
 
