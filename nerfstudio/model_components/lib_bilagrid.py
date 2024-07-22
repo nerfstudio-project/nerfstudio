ff3b6f66d25b9071030e5abca75c238f368005a6
xucr <xucr@shanghaitech.edu.cn>
2024-07-22 13:45:22 +0800

    fix

--- nerfstudio/model_components/lib_bilagrid.py
+++ nerfstudio/model_components/lib_bilagrid.py
@@ -1,4 +1,4 @@
-# Copyright 2022 the Regents of the University of California, Nerfstudio Team and contributors. All rights reserved.
+# This part of code was borrowed form ["Bilateral Guided Radiance Field Processing"](https://bilarfpro.github.io/).
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
