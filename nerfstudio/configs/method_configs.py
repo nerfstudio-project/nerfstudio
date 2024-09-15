f6dd7dd89c0d0b7fe30d75a1b95e44290167c428
AntonioMacaronio <AntZhan1234@gmail.com>
2024-09-14 18:01:16 -0700

    removing oom methods, adding the ability to add a flag to dataloading

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -95,6 +95,7 @@ method_configs["nerfacto"] = TrainerConfig(
             dataparser=NerfstudioDataParserConfig(),
             train_num_rays_per_batch=4096,
             eval_num_rays_per_batch=4096,
+            load_from_disk=True,
         ),
         model=NerfactoModelConfig(
             eval_num_rays_per_chunk=1 << 15,
@@ -202,48 +203,6 @@ method_configs["nerfacto-huge"] = TrainerConfig(
         "fields": {
             "optimizer": RAdamOptimizerConfig(lr=1e-2, eps=1e-15),
             "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-4, max_steps=50000),
-        },
-        "camera_opt": {
-            "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
-            "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-4, max_steps=5000),
-        },
-    },
-    viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
-    vis="viewer",
-)
-
-method_configs["nerfacto-oom"] = TrainerConfig(
-    method_name="nerfacto-oom",
-    steps_per_eval_batch=500,
-    steps_per_save=2000,
-    max_num_iterations=30000,
-    mixed_precision=True,
-    pipeline=VanillaPipelineConfig(
-        datamanager=VanillaDataManagerConfig(
-            dataparser=NerfstudioDataParserConfig(),
-            train_num_rays_per_batch=4096,
-            eval_num_rays_per_batch=4096,
-            use_parallel_dataloader=True,
-            load_from_disk=True,
-            dataloader_num_workers=4,
-            prefetch_factor=10,
-            train_num_images_to_sample_from=50,
-            train_num_times_to_repeat_images=10,
-        ),
-        model=NerfactoModelConfig(
-            eval_num_rays_per_chunk=1 << 15,
-            average_init_density=0.01,
-            camera_optimizer=CameraOptimizerConfig(mode="SO3xR3"),
-        ),
-    ),
-    optimizers={
-        "proposal_networks": {
-            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-            "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
-        },
-        "fields": {
-            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
-            "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
         },
         "camera_opt": {
             "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
@@ -706,60 +665,6 @@ method_configs["splatfacto-big"] = TrainerConfig(
             continue_cull_post_densification=False,
             densify_grad_thresh=0.0006,
         ),
-    ),
-    optimizers={
-        "means": {
-            "optimizer": AdamOptimizerConfig(lr=1.6e-4, eps=1e-15),
-            "scheduler": ExponentialDecaySchedulerConfig(
-                lr_final=1.6e-6,
-                max_steps=30000,
-            ),
-        },
-        "features_dc": {
-            "optimizer": AdamOptimizerConfig(lr=0.0025, eps=1e-15),
-            "scheduler": None,
-        },
-        "features_rest": {
-            "optimizer": AdamOptimizerConfig(lr=0.0025 / 20, eps=1e-15),
-            "scheduler": None,
-        },
-        "opacities": {
-            "optimizer": AdamOptimizerConfig(lr=0.05, eps=1e-15),
-            "scheduler": None,
-        },
-        "scales": {
-            "optimizer": AdamOptimizerConfig(lr=0.005, eps=1e-15),
-            "scheduler": None,
-        },
-        "quats": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
-        "camera_opt": {
-            "optimizer": AdamOptimizerConfig(lr=1e-4, eps=1e-15),
-            "scheduler": ExponentialDecaySchedulerConfig(
-                lr_final=5e-7, max_steps=30000, warmup_steps=1000, lr_pre_warmup=0
-            ),
-        },
-    },
-    viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
-    vis="viewer",
-)
-
-method_configs["splatfacto-oom"] = TrainerConfig(
-    method_name="splatfacto-oom",
-    steps_per_eval_image=100,
-    steps_per_eval_batch=0,
-    steps_per_save=2000,
-    steps_per_eval_all_images=1000,
-    max_num_iterations=30000,
-    mixed_precision=False,
-    pipeline=VanillaPipelineConfig(
-        datamanager=FullImageDatamanagerConfig(
-            # _target=ParallelFullImageDatamanager[InputDataset],
-            dataparser=NerfstudioDataParserConfig(load_3D_points=True, downscale_factor=1),
-            # dataparser=NerfstudioDataParserConfig(load_3D_points=True),
-            cache_images_type="uint8",
-            use_parallel_dataloader=True,
-        ),
-        model=SplatfactoModelConfig(),
     ),
     optimizers={
         "means": {
