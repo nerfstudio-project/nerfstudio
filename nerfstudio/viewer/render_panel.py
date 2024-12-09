02b3837b115479901eaf404f4dea93fd7df737e7
Yudong Jin <krahets@163.com>
2024-12-09 20:08:10 +0800

    Update render_panel.py

--- nerfstudio/viewer/render_panel.py
+++ nerfstudio/viewer/render_panel.py
@@ -28,10 +28,9 @@ import splines
 import splines.quaternion
 import viser
 import viser.transforms as tf
-from scipy import interpolate
-
 from nerfstudio.utils.rich_utils import CONSOLE
 from nerfstudio.viewer.control_panel import ControlPanel
+from scipy import interpolate
 
 @dataclasses.dataclass
 class Keyframe:
